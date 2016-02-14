51d327113893094a77badc60bbc9b41731cd0075
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-14 17:50:57 +0100

    Basic test and data file

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.gutenbergde/src/test/java/de/unistuttgart/quadrama/io/gutenbergde/TestGutenbergDEReader.java
@@ -0,0 +1,39 @@
+package de.unistuttgart.quadrama.io.gutenbergde;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.unistuttgart.quadrama.api.Act;
+
+public class TestGutenbergDEReader {
+
+	CollectionReaderDescription description;
+
+	@Before
+	public void setUp() throws ResourceInitializationException {
+		description =
+				CollectionReaderFactory.createReaderDescription(
+						GutenbergDEReader.class,
+						GutenbergDEReader.PARAM_INPUT_DIRECTORY,
+						"src/test/resources/test1");
+	}
+
+	@Test
+	public void testReader() throws UIMAException, IOException {
+		JCas jcas =
+				SimplePipeline.iteratePipeline(description).iterator().next();
+
+		assertTrue(JCasUtil.exists(jcas, Act.class));
+	}
+}
