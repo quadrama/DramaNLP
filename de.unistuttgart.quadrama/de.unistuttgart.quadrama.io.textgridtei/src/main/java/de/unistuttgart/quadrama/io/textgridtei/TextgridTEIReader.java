2dc6484165ee62f4afd9e33886f8249e395a82f1
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 15:57:01 +0100

    initial implementation to import TEI data

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.textgridtei/src/main/java/de/unistuttgart/quadrama/io/textgridtei/TextgridTEIReader.java
@@ -0,0 +1,71 @@
+package de.unistuttgart.quadrama.io.textgridtei;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import de.unistuttgart.quadrama.api.Act;
+import de.unistuttgart.quadrama.api.Drama;
+import de.unistuttgart.quadrama.api.DramatisPersonae;
+import de.unistuttgart.quadrama.api.FrontMatter;
+import de.unistuttgart.quadrama.api.MainMatter;
+import de.unistuttgart.quadrama.api.Scene;
+import de.unistuttgart.quadrama.api.Speaker;
+import de.unistuttgart.quadrama.api.Speech;
+import de.unistuttgart.quadrama.api.StageDirection;
+import de.unistuttgart.quadrama.api.Utterance;
+import de.unistuttgart.quadrama.io.gutenbergde.AbstractDramaReader;
+
+public class TextgridTEIReader extends AbstractDramaReader {
+
+	@Override
+	public void getNext(JCas jcas) throws IOException, CollectionException {
+		Drama drama = new Drama(jcas);
+		drama.setDocumentId("test");
+		drama.addToIndexes();
+		jcas.setDocumentLanguage("de");
+
+		File file = files[current++];
+
+		String str = IOUtils.toString(new FileInputStream(file));
+		Document doc = Jsoup.parse(str, "", Parser.xmlParser());
+
+		Visitor vis = new Visitor(jcas);
+		doc.traverse(vis);
+		jcas = vis.getJCas();
+
+		selectRange2Annotation(jcas, doc, vis.getAnnotationMap(), "title",
+				"front", FrontMatter.class);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "body",
+				MainMatter.class, null);
+
+		FrontMatter frontMatter =
+				JCasUtil.selectSingle(jcas, FrontMatter.class);
+		MainMatter mainMatter = JCasUtil.selectSingle(jcas, MainMatter.class);
+
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "speaker",
+				Speaker.class, null);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "stage",
+				StageDirection.class, mainMatter);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "sp",
+				Utterance.class, null);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "l", Speech.class,
+				mainMatter);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "p", Speech.class,
+				mainMatter);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "div[type=scene]",
+				Scene.class, null);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(), "body > div",
+				Act.class, null);
+		select2Annotation(jcas, doc, vis.getAnnotationMap(),
+				"front div:has(p)", DramatisPersonae.class, frontMatter);
+	}
+}
---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.textgridtei/src/test/java/de/unistuttgart/quadrama/io/textgridtei/TestTextgridTEIReader.java
@@ -0,0 +1,62 @@
+package de.unistuttgart.quadrama.io.textgridtei;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.unistuttgart.quadrama.api.Act;
+import de.unistuttgart.quadrama.api.Drama;
+import de.unistuttgart.quadrama.api.DramatisPersonae;
+import de.unistuttgart.quadrama.api.FrontMatter;
+import de.unistuttgart.quadrama.api.MainMatter;
+import de.unistuttgart.quadrama.api.Scene;
+import de.unistuttgart.quadrama.api.Speaker;
+
+public class TestTextgridTEIReader {
+
+	CollectionReaderDescription description;
+
+	@Before
+	public void setUp() throws ResourceInitializationException {
+		description =
+				CollectionReaderFactory.createReaderDescription(
+						TextgridTEIReader.class,
+						TextgridTEIReader.PARAM_INPUT_DIRECTORY,
+						"src/test/resources/");
+	}
+
+	@Test
+	public void testReader() throws UIMAException, IOException {
+		JCas jcas =
+				SimplePipeline
+						.iteratePipeline(
+								description,
+								AnalysisEngineFactory.createEngineDescription(
+										XmiWriter.class,
+										XmiWriter.PARAM_TARGET_LOCATION,
+										"target/doc")).iterator().next();
+
+		assertNotNull(JCasUtil.selectSingle(jcas, Drama.class));
+		assertTrue(JCasUtil.exists(jcas, Act.class));
+		assertTrue(JCasUtil.exists(jcas, Scene.class));
+		assertTrue(JCasUtil.exists(jcas, Speaker.class));
+		assertTrue(JCasUtil.exists(jcas, DramatisPersonae.class));
+		assertNotNull(JCasUtil.selectSingle(jcas, FrontMatter.class));
+		assertNotNull(JCasUtil.selectSingle(jcas, MainMatter.class));
+		assertEquals(5, JCasUtil.select(jcas, Act.class).size());
+	}
+}
