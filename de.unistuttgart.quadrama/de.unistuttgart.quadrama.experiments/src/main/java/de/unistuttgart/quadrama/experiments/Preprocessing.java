9560a2713affcbb82f91f85de2f10e9ae2279987
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 17:21:51 +0100

    added pos tagging and parsing

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/Preprocessing.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/Preprocessing.java
@@ -1,10 +1,11 @@
 package de.unistuttgart.quadrama.experiments;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.collection.CollectionReaderDescription;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ResourceInitializationException;
@@ -12,6 +13,8 @@ import org.apache.uima.resource.ResourceInitializationException;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
 import de.unistuttgart.quadrama.core.DramaSpeechSegmenter;
 
 public class Preprocessing {
@@ -22,9 +25,15 @@ public class Preprocessing {
 						XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
 						"src/main/resources/romeo-and-juliet/de/*.xmi");
 
-		SimplePipeline.runPipeline(crd, DramaSpeechSegmenter
+		SimplePipeline
+		.runPipeline(
+				crd,
+				createEngineDescription(FixGutenbergSpeech.class),
+				DramaSpeechSegmenter
 				.getWrappedSegmenterDescription(LanguageToolSegmenter.class),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class,
+				createEngineDescription(StanfordPosTagger.class),
+				createEngineDescription(StanfordParser.class),
+				createEngineDescription(XmiWriter.class,
 						XmiWriter.PARAM_TARGET_LOCATION, "target/"));
 	}
 }
