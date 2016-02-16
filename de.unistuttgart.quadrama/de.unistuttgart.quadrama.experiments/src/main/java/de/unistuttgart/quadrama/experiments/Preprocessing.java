7f746e40fd19a45cbb795b049aa58ea727f72951
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 17:11:56 +0100

    added preprocessing step and base data

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/Preprocessing.java
@@ -0,0 +1,30 @@
+package de.unistuttgart.quadrama.experiments;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
+import de.unistuttgart.quadrama.core.DramaSpeechSegmenter;
+
+public class Preprocessing {
+	public static void main(String[] args)
+			throws ResourceInitializationException, UIMAException, IOException {
+		CollectionReaderDescription crd =
+				CollectionReaderFactory.createReaderDescription(
+						XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
+						"src/main/resources/romeo-and-juliet/de/*.xmi");
+
+		SimplePipeline.runPipeline(crd, DramaSpeechSegmenter
+				.getWrappedSegmenterDescription(LanguageToolSegmenter.class),
+				AnalysisEngineFactory.createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION, "target/"));
+	}
+}
