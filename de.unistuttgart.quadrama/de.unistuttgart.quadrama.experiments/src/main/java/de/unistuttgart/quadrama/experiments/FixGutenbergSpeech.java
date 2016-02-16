c7692bc04ccc7e95faa7842297e979d611a4efd6
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 17:20:00 +0100

    added component to fix the issue with the period in Gutenberg

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/FixGutenbergSpeech.java
@@ -0,0 +1,25 @@
+package de.unistuttgart.quadrama.experiments;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.unistuttgart.quadrama.api.Speech;
+import de.unistuttgart.quadrama.api.Utterance;
+
+public class FixGutenbergSpeech extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		for (Utterance utterance : JCasUtil.select(jcas, Utterance.class)) {
+			Speech speech =
+					JCasUtil.selectCovered(Speech.class, utterance).iterator()
+					.next();
+			if (speech.getCoveredText().startsWith(". ")) {
+				speech.setBegin(speech.getBegin() + 2);
+			}
+		}
+	}
+
+}
