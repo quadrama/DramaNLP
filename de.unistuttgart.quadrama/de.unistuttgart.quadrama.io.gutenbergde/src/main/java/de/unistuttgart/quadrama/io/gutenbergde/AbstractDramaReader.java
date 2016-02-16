d4b5bc27b5b6e3395667e90b48bd1cd98ab908fc
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 15:35:53 +0100

    moved some code in an abstract super class

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.gutenbergde/src/main/java/de/unistuttgart/quadrama/io/gutenbergde/AbstractDramaReader.java
@@ -0,0 +1,136 @@
+package de.unistuttgart.quadrama.io.gutenbergde;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.factory.JCasBuilder;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Progress;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import org.jsoup.select.NodeVisitor;
+
+import de.unistuttgart.quadrama.io.gutenbergde.type.HTMLAnnotation;
+
+public abstract class AbstractDramaReader extends JCasCollectionReader_ImplBase {
+	public static final String PARAM_INPUT_DIRECTORY = "Input Directory";
+
+	@ConfigurationParameter(name = PARAM_INPUT_DIRECTORY, mandatory = true)
+	String inputDirectory;
+
+	File[] files;
+
+	int current = 0;
+
+	@Override
+	public void initialize(UimaContext context)
+			throws ResourceInitializationException {
+		super.initialize(context);
+
+		File inputDir = new File(inputDirectory);
+		files = inputDir.listFiles();
+	}
+
+	public boolean hasNext() throws IOException, CollectionException {
+		return current < files.length - 1;
+	}
+
+	public Progress[] getProgress() {
+		// TODO: implement
+		return null;
+	}
+
+	public <T extends Annotation> Collection<T> select2Annotation(JCas jcas,
+			Element rootElement, Map<String, HTMLAnnotation> annoMap, String cssSelector, Class<T> annoClass, Annotation coveringAnnotation) {
+				HashSet<T> set = new HashSet<T>();
+				Elements elms = rootElement.select(cssSelector);
+				for (Element elm : elms) {
+					HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
+					if (coveringAnnotation == null
+							|| (coveringAnnotation.getBegin() <= hAnno.getBegin() && coveringAnnotation
+									.getEnd() >= hAnno.getEnd()))
+						set.add(AnnotationFactory.createAnnotation(jcas,
+								hAnno.getBegin(), hAnno.getEnd(), annoClass));
+				}
+				return set;
+			}
+
+	public <T extends Annotation> T selectRange2Annotation(JCas jcas,
+			Element rootElement, Map<String, HTMLAnnotation> annoMap, String beginCssSelector, String endCssSelector, Class<T> annoClass) {
+				Elements elms = rootElement.select(beginCssSelector);
+				int begin = jcas.size();
+				for (Element elm : elms) {
+					HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
+					if (hAnno.getBegin() < begin) begin = hAnno.getBegin();
+				}
+			
+				elms = rootElement.select(endCssSelector);
+				int end = 0;
+				for (Element elm : elms) {
+					HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
+					if (hAnno.getEnd() > end) end = hAnno.getEnd();
+				}
+			
+				return AnnotationFactory.createAnnotation(jcas, begin, end, annoClass);
+			}
+
+	public class Visitor implements NodeVisitor {
+
+		JCasBuilder builder;
+		Map<Node, Integer> beginMap = new HashMap<Node, Integer>();
+
+		Map<String, HTMLAnnotation> annotationMap =
+				new HashMap<String, HTMLAnnotation>();
+
+		public Visitor(JCas jcas) {
+			builder = new JCasBuilder(jcas);
+		}
+
+		public void head(Node node, int depth) {
+			if (node.getClass().equals(TextNode.class)) {
+				builder.add(((TextNode) node).text());
+			} else {
+				beginMap.put(node, builder.getPosition());
+			}
+		}
+
+		public void tail(Node node, int depth) {
+			if (node.getClass().equals(Element.class)) {
+				Element elm = (Element) node;
+				HTMLAnnotation anno =
+						builder.add(beginMap.get(node), HTMLAnnotation.class);
+				anno.setTag(elm.tagName());
+				anno.setId(elm.id());
+				if (elm.className().isEmpty())
+					anno.setCls(elm.attr("type"));
+				else
+					anno.setCls(elm.className());
+				annotationMap.put(elm.cssSelector(), anno);
+				if (elm.isBlock()) builder.add("\n");
+			}
+		}
+
+		public JCas getJCas() {
+			builder.close();
+			return builder.getJCas();
+		}
+
+		public Map<String, HTMLAnnotation> getAnnotationMap() {
+			return annotationMap;
+		}
+	}
+
+}
--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.gutenbergde/src/main/java/de/unistuttgart/quadrama/io/gutenbergde/GutenbergDEReader.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.gutenbergde/src/main/java/de/unistuttgart/quadrama/io/gutenbergde/GutenbergDEReader.java
@@ -3,31 +3,18 @@ package de.unistuttgart.quadrama.io.gutenbergde;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeSet;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.uima.UimaContext;
 import org.apache.uima.collection.CollectionException;
-import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.factory.AnnotationFactory;
-import org.apache.uima.fit.factory.JCasBuilder;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.util.Progress;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.select.Elements;
-import org.jsoup.select.NodeVisitor;
 
 import de.unistuttgart.quadrama.api.Act;
 import de.unistuttgart.quadrama.api.Drama;
@@ -42,34 +29,7 @@ import de.unistuttgart.quadrama.api.StageDirection;
 import de.unistuttgart.quadrama.api.Utterance;
 import de.unistuttgart.quadrama.io.gutenbergde.type.HTMLAnnotation;
 
-public class GutenbergDEReader extends JCasCollectionReader_ImplBase {
-
-	public static final String PARAM_INPUT_DIRECTORY = "Input Directory";
-
-	@ConfigurationParameter(name = PARAM_INPUT_DIRECTORY, mandatory = true)
-	String inputDirectory;
-
-	File[] files;
-
-	int current = 0;
-
-	@Override
-	public void initialize(UimaContext context)
-			throws ResourceInitializationException {
-		super.initialize(context);
-
-		File inputDir = new File(this.inputDirectory);
-		files = inputDir.listFiles();
-	}
-
-	public boolean hasNext() throws IOException, CollectionException {
-		return current < files.length - 1;
-	}
-
-	public Progress[] getProgress() {
-		// TODO: implement
-		return null;
-	}
+public class GutenbergDEReader extends AbstractDramaReader {
 
 	@Override
 	public void getNext(JCas jcas) throws IOException, CollectionException {
@@ -167,43 +127,6 @@ public class GutenbergDEReader extends JCasCollectionReader_ImplBase {
 		}
 	}
 
-	public <T extends Annotation> Collection<T> select2Annotation(JCas jcas,
-			Element rootElement, Map<String, HTMLAnnotation> annoMap,
-			String cssSelector, Class<T> annoClass,
-			Annotation coveringAnnotation) {
-		HashSet<T> set = new HashSet<T>();
-		Elements elms = rootElement.select(cssSelector);
-		for (Element elm : elms) {
-			HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
-			if (coveringAnnotation == null
-					|| (coveringAnnotation.getBegin() <= hAnno.getBegin() && coveringAnnotation
-							.getEnd() >= hAnno.getEnd()))
-				set.add(AnnotationFactory.createAnnotation(jcas,
-						hAnno.getBegin(), hAnno.getEnd(), annoClass));
-		}
-		return set;
-	}
-
-	public <T extends Annotation> T selectRange2Annotation(JCas jcas,
-			Element rootElement, Map<String, HTMLAnnotation> annoMap,
-			String beginCssSelector, String endCssSelector, Class<T> annoClass) {
-		Elements elms = rootElement.select(beginCssSelector);
-		int begin = jcas.size();
-		for (Element elm : elms) {
-			HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
-			if (hAnno.getBegin() < begin) begin = hAnno.getBegin();
-		}
-
-		elms = rootElement.select(endCssSelector);
-		int end = 0;
-		for (Element elm : elms) {
-			HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
-			if (hAnno.getEnd() > end) end = hAnno.getEnd();
-		}
-
-		return AnnotationFactory.createAnnotation(jcas, begin, end, annoClass);
-	}
-
 	protected void assignSpeakerIds(JCas jcas) {
 		DramatisPersonae dp =
 				JCasUtil.selectSingle(jcas, DramatisPersonae.class);
@@ -224,49 +147,6 @@ public class GutenbergDEReader extends JCasCollectionReader_ImplBase {
 					// no entry in speaker map
 				}
 			}
-		}
-	}
-
-	public class Visitor implements NodeVisitor {
-
-		JCasBuilder builder;
-		Map<Node, Integer> beginMap = new HashMap<Node, Integer>();
-
-		Map<String, HTMLAnnotation> annotationMap =
-				new HashMap<String, HTMLAnnotation>();
-
-		public Visitor(JCas jcas) {
-			builder = new JCasBuilder(jcas);
-		}
-
-		public void head(Node node, int depth) {
-			if (node.getClass().equals(TextNode.class)) {
-				builder.add(((TextNode) node).text());
-			} else {
-				beginMap.put(node, builder.getPosition());
-			}
-		}
-
-		public void tail(Node node, int depth) {
-			if (node.getClass().equals(Element.class)) {
-				Element elm = (Element) node;
-				HTMLAnnotation anno =
-						builder.add(beginMap.get(node), HTMLAnnotation.class);
-				anno.setTag(elm.tagName());
-				anno.setId(elm.id());
-				anno.setCls(elm.className());
-				annotationMap.put(elm.cssSelector(), anno);
-				if (elm.isBlock()) builder.add("\n");
-			}
-		}
-
-		public JCas getJCas() {
-			builder.close();
-			return builder.getJCas();
-		}
-
-		public Map<String, HTMLAnnotation> getAnnotationMap() {
-			return annotationMap;
 		}
 	}
 
