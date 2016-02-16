efbaa2ddf2ea33b673c0dbfb139b7fcba85f9f7c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-16 14:08:19 +0100

    Now we have a system of apply arbitrary DKpro components on just the Speech content of the drama

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.core/src/main/java/de/unistuttgart/quadrama/core/DramaSpeechPostProcessing.java
@@ -0,0 +1,66 @@
+package de.unistuttgart.quadrama.core;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.unistuttgart.quadrama.core.api.Origin;
+
+public class DramaSpeechPostProcessing extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+		try {
+			// map tokens
+			JCas utteranceCas = jcas.getView("Utterances");
+			for (Origin origin : JCasUtil.select(utteranceCas, Origin.class)) {
+				for (Token token : JCasUtil.selectCovered(Token.class, origin)) {
+					int begin =
+							token.getBegin() + origin.getOffset()
+									- origin.getBegin();
+					int end =
+							token.getEnd() + origin.getOffset()
+									- origin.getBegin();
+					AnnotationFactory.createAnnotation(jcas, begin, end,
+							Token.class);
+				}
+			}
+
+			// map sentences
+			Map<Token, Collection<Origin>> covers =
+					JCasUtil.indexCovering(utteranceCas, Token.class,
+							Origin.class);
+			for (Sentence sentence : JCasUtil.select(utteranceCas,
+					Sentence.class)) {
+				List<Token> tokens =
+						JCasUtil.selectCovered(Token.class, sentence);
+				Token firstToken = tokens.get(0);
+				Token lastToken = tokens.get(tokens.size() - 1);
+				Origin firstOrigin = covers.get(firstToken).iterator().next();
+				int begin =
+						sentence.getBegin() + firstOrigin.getOffset()
+						- firstOrigin.getBegin();
+				Origin lastOrigin = covers.get(lastToken).iterator().next();
+				int end =
+						sentence.getEnd() + lastOrigin.getOffset()
+						- lastOrigin.getBegin();
+				AnnotationFactory.createAnnotation(jcas, begin, end,
+						Sentence.class);
+			}
+
+		} catch (CASException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+
+	}
+}
---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.core/src/main/java/de/unistuttgart/quadrama/core/DramaSpeechPreparation.java
@@ -0,0 +1,38 @@
+package de.unistuttgart.quadrama.core;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.factory.JCasBuilder;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.unistuttgart.quadrama.api.Speech;
+import de.unistuttgart.quadrama.api.Utterance;
+import de.unistuttgart.quadrama.core.api.Origin;
+
+public class DramaSpeechPreparation extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		JCas utteranceCas;
+		try {
+			utteranceCas = jcas.createView("Utterances");
+			utteranceCas.setDocumentLanguage(jcas.getDocumentLanguage());
+			JCasBuilder b = new JCasBuilder(utteranceCas);
+			for (Utterance utterance : JCasUtil.select(jcas, Utterance.class)) {
+				for (Speech speech : JCasUtil.selectCovered(Speech.class,
+						utterance)) {
+					b.add(speech.getCoveredText(), Origin.class).setOffset(
+							speech.getBegin());
+				}
+				b.add("\n\n\n");
+			}
+			b.close();
+		} catch (CASException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+}
