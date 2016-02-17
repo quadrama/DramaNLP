54384be83651c301a4a9e850909e4b1929e54b4f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-17 09:34:00 +0100

    simple component

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.graph/src/main/java/de/unistuttgart/quadrama/graph/NetworkExtractor.java
@@ -0,0 +1,16 @@
+package de.unistuttgart.quadrama.graph;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.jcas.JCas;
+
+public class NetworkExtractor extends JCasAnnotator_ImplBase {
+
+	public static final String PARAM_BYACT = "By Act";
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+	}
+
+}
