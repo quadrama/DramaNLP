99afc9397c3d422b0e0472ce8d3f6e7c8ad3caa9
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-18 17:51:01 +0100

    added MentionNetworkExtractor to detect whether someone is mentioned

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/Visualise.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/Visualise.java
@@ -14,9 +14,11 @@ import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.tudarmstadt.ukp.dkpro.core.languagetool.LanguageToolSegmenter;
 import de.unistuttgart.quadrama.core.DramaSpeechSegmenter;
+import de.unistuttgart.quadrama.core.FigureMentionDetection;
 import de.unistuttgart.quadrama.core.SpeakerIdentifier;
 import de.unistuttgart.quadrama.graph.NetworkExtractor;
 import de.unistuttgart.quadrama.io.dot.DotExporter;
+import de.unistuttgart.quadrama.io.dot.MentionNetworkExtractor;
 
 public class Visualise {
 
@@ -31,17 +33,21 @@ public class Visualise {
 						"src/main/resources/romeo-and-juliet/de/*.xmi");
 
 		SimplePipeline
-		.runPipeline(
-				crd,
-				createEngineDescription(FixGutenbergSpeech.class),
-				DramaSpeechSegmenter
-				.getWrappedSegmenterDescription(LanguageToolSegmenter.class),
-				createEngineDescription(SpeakerIdentifier.class),
-				createEngineDescription(NetworkExtractor.class),
-				createEngineDescription(XmiWriter.class,
-								XmiWriter.PARAM_TARGET_LOCATION, "target/xmi/"),
+				.runPipeline(
+						crd,
+						createEngineDescription(FixGutenbergSpeech.class),
+						DramaSpeechSegmenter
+								.getWrappedSegmenterDescription(LanguageToolSegmenter.class),
+						createEngineDescription(SpeakerIdentifier.class),
+						createEngineDescription(FigureMentionDetection.class),
+						createEngineDescription(NetworkExtractor.class),
+						createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION, "target/xmi/"),
 						createEngineDescription(DotExporter.class,
 								DotExporter.PARAM_TARGET_LOCATION,
-								"target/dot/"));
+								"target/dot/"),
+								createEngineDescription(MentionNetworkExtractor.class,
+										DotExporter.PARAM_TARGET_LOCATION,
+										"target/dot2/"));
 	}
 }
---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.dot/src/main/java/de/unistuttgart/quadrama/io/dot/MentionNetworkExtractor.java
@@ -0,0 +1,74 @@
+package de.unistuttgart.quadrama.io.dot;
+
+import static org.apache.commons.io.IOUtils.closeQuietly;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.ext.DOTExporter;
+import org.jgrapht.ext.VertexNameProvider;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedPseudograph;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+import de.unistuttgart.quadrama.api.Figure;
+import de.unistuttgart.quadrama.api.FigureMention;
+import de.unistuttgart.quadrama.api.Speaker;
+import de.unistuttgart.quadrama.api.Utterance;
+
+public class MentionNetworkExtractor extends JCasFileWriter_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		DirectedGraph<Figure, DefaultEdge> graph =
+				new DirectedPseudograph<Figure, DefaultEdge>(DefaultEdge.class);
+
+		for (Utterance utterance : JCasUtil.select(jcas, Utterance.class)) {
+			Speaker speaker =
+					JCasUtil.selectCovered(Speaker.class, utterance).get(0);
+			for (FigureMention mention : JCasUtil.selectCovered(jcas,
+					FigureMention.class, utterance)) {
+				if (speaker.getFigure() != null && mention.getFigure() != null) {
+					if (!graph.containsVertex(speaker.getFigure()))
+						graph.addVertex(speaker.getFigure());
+					if (!graph.containsVertex(mention.getFigure()))
+						graph.addVertex(mention.getFigure());
+					graph.addEdge(speaker.getFigure(), mention.getFigure());
+				}
+			}
+		}
+
+		OutputStream docOS = null;
+		OutputStreamWriter writer = null;
+		try {
+			docOS = getOutputStream(jcas, ".dot");
+			writer = new OutputStreamWriter(docOS);
+			DOTExporter<Figure, DefaultEdge> exporter =
+					new DOTExporter<Figure, DefaultEdge>(
+							new VertexNameProvider<Figure>() {
+
+								public String getVertexName(Figure vertex) {
+									return String.valueOf(vertex.getId());
+								}
+							}, new VertexNameProvider<Figure>() {
+
+								public String getVertexName(Figure vertex) {
+									return vertex.getCoveredText();
+								}
+							}, null);
+			exporter.export(writer, graph);
+
+		} catch (Exception e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			closeQuietly(writer);
+			closeQuietly(docOS);
+
+		}
+	}
+
+}
