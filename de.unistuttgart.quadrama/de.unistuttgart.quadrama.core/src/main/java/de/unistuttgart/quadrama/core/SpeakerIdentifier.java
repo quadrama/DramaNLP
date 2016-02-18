2990c0b3a43d25ed7e4666bbda0c0026a08b5628
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-18 16:44:39 +0100

    added logging and improved the speaker identification strategy

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.core/src/main/java/de/unistuttgart/quadrama/core/SpeakerIdentifier.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.core/src/main/java/de/unistuttgart/quadrama/core/SpeakerIdentifier.java
@@ -3,18 +3,29 @@ package de.unistuttgart.quadrama.core;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.Level;
 
+import de.unistuttgart.quadrama.api.Drama;
 import de.unistuttgart.quadrama.api.Figure;
 import de.unistuttgart.quadrama.api.Speaker;
 
 public class SpeakerIdentifier extends JCasAnnotator_ImplBase {
 
+	int threshold = 2;
+
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		getLogger().log(
+				Level.FINE,
+				"Now processing "
+						+ JCasUtil.selectSingle(jcas, Drama.class)
+								.getDocumentId());
+
 		Map<String, Figure> map = new HashMap<String, Figure>();
 		int figureId = 0;
 		for (Figure figure : JCasUtil.select(jcas, Figure.class)) {
@@ -29,7 +40,38 @@ public class SpeakerIdentifier extends JCasAnnotator_ImplBase {
 			if (map.containsKey(sName))
 				cm.setFigure(map.get(sName));
 			else {
-				System.err.println("Could not assign " + sName);
+				for (Figure figure : map.values()) {
+					if (figure.getCoveredText().trim().toLowerCase()
+							.contains(sName)) {
+						cm.setFigure(figure);
+						break;
+					}
+				}
+				if (cm.getFigure() == null)
+					for (Figure figure : map.values()) {
+						if (figure.getDescription() != null
+								&& figure.getDescription().toLowerCase().trim()
+								.contains(sName)) {
+							cm.setFigure(figure);
+							break;
+						}
+					}
+				if (cm.getFigure() == null) {
+					for (Figure figure : map.values()) {
+						int lev =
+								StringUtils.getLevenshteinDistance(figure
+										.getCoveredText().trim().toLowerCase(),
+										sName);
+						if (lev <= threshold) {
+							cm.setFigure(figure);
+							break;
+						}
+
+					}
+				}
+				if (cm.getFigure() == null) {
+					getLogger().log(Level.INFO, "Could not assign " + sName);
+				}
 			}
 		}
 
