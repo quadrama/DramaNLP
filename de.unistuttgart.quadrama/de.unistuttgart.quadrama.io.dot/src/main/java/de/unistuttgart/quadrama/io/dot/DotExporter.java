0594443bda09ffca2f2943776bcd40087111c685
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-02-18 15:54:02 +0100

    Now using Figure annotation in graph

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.dot/src/main/java/de/unistuttgart/quadrama/io/dot/DotExporter.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.dot/src/main/java/de/unistuttgart/quadrama/io/dot/DotExporter.java
@@ -4,82 +4,62 @@ import static org.apache.commons.io.IOUtils.closeQuietly;
 
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.cas.CASException;
 import org.apache.uima.jcas.JCas;
+import org.jgrapht.WeightedGraph;
 import org.jgrapht.ext.DOTExporter;
 import org.jgrapht.ext.VertexNameProvider;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleWeightedGraph;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
-import de.unistuttgart.quadrama.api.Scene;
-import de.unistuttgart.quadrama.api.Speaker;
+import de.unistuttgart.quadrama.api.Figure;
+import de.unistuttgart.quadrama.graph.NetworkExtractor;
+import de.unistuttgart.quadrama.graph.ext.GraphImporter;
 
 public class DotExporter extends JCasFileWriter_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		SimpleWeightedGraph<Integer, DefaultWeightedEdge> graph =
-				new SimpleWeightedGraph<Integer, DefaultWeightedEdge>(
-						DefaultWeightedEdge.class);
-		final Map<Integer, String> nameMap = new HashMap<Integer, String>();
-		for (Speaker speaker : JCasUtil.select(jcas, Speaker.class)) {
-			if (!graph.containsVertex(speaker.getId())) {
-				graph.addVertex(speaker.getId());
-				nameMap.put(speaker.getId(), speaker.getCoveredText());
-			}
-		}
-
-		for (Scene scene : JCasUtil.select(jcas, Scene.class)) {
-			List<Speaker> speakers =
-					JCasUtil.selectCovered(Speaker.class, scene);
-			for (Speaker s1 : speakers) {
-				for (Speaker s2 : speakers) {
-					if (graph.containsEdge(s1.getId(), s2.getId())) {
-						DefaultWeightedEdge edge =
-								graph.getEdge(s1.getId(), s2.getId());
-						double w = graph.getEdgeWeight(edge);
-						graph.setEdgeWeight(edge, w + 1.0);
-					} else {
-						if (s1.getId() != s2.getId())
-							graph.addEdge(s1.getId(), s2.getId());
-					}
-				}
-			};
-		}
-
-		OutputStream docOS = null;
-		OutputStreamWriter writer = null;
+		JCas view;
 		try {
-			docOS = getOutputStream(jcas, ".dot");
-			writer = new OutputStreamWriter(docOS);
-			DOTExporter<Integer, DefaultWeightedEdge> exporter =
-					new DOTExporter<Integer, DefaultWeightedEdge>(
-							new VertexNameProvider<Integer>() {
+			view = jcas.getView(NetworkExtractor.NETWORK_VIEW);
 
-								public String getVertexName(Integer vertex) {
-									return String.valueOf(vertex.hashCode());
-								}
-							}, new VertexNameProvider<Integer>() {
+			WeightedGraph<Figure, DefaultWeightedEdge> graph =
+					GraphImporter.getGraph(view.getDocumentText(), jcas);
 
-								public String getVertexName(Integer vertex) {
-									return nameMap.get(vertex);
-								}
-							}, null);
-			exporter.export(writer, graph);
+			OutputStream docOS = null;
+			OutputStreamWriter writer = null;
+			try {
+				docOS = getOutputStream(jcas, ".dot");
+				writer = new OutputStreamWriter(docOS);
+				DOTExporter<Figure, DefaultWeightedEdge> exporter =
+						new DOTExporter<Figure, DefaultWeightedEdge>(
+								new VertexNameProvider<Figure>() {
 
-		} catch (Exception e) {
-			throw new AnalysisEngineProcessException(e);
-		} finally {
-			closeQuietly(writer);
-			closeQuietly(docOS);
+									public String getVertexName(Figure vertex) {
+										return String.valueOf(vertex.getId());
+									}
+								}, new VertexNameProvider<Figure>() {
 
+									public String getVertexName(Figure vertex) {
+										return vertex.getCoveredText();
+									}
+								}, null);
+				exporter.export(writer, graph);
+
+			} catch (Exception e) {
+				throw new AnalysisEngineProcessException(e);
+			} finally {
+				closeQuietly(writer);
+				closeQuietly(docOS);
+
+			}
+		} catch (CASException e1) {
+			throw new AnalysisEngineProcessException(e1);
 		}
+
 	}
 }
