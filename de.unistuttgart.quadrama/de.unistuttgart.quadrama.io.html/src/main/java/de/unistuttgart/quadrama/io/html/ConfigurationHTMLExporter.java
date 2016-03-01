4cb0070f341928340c1719c79b50dd5b7c38ebca
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 16:27:25 +0100

    basic html export module

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
@@ -0,0 +1,15 @@
+package de.unistuttgart.quadrama.io.html;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+
+public class ConfigurationHTMLExporter extends JCasFileWriter_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+	}
+
+}
