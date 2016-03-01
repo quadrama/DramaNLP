6d4d15ae97981f363af74dabcface397c03f2a75
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 16:46:38 +0100

    We now copy resource correctly into the output

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
@@ -1,8 +1,11 @@
 package de.unistuttgart.quadrama.io.html;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -13,10 +16,12 @@ import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
 
 public class ConfigurationHTMLExporter extends JCasFileWriter_ImplBase {
 
+	Set<String> names = new HashSet<String>();
+
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		JSONObject object = null;
-		OutputStream os = null;
+		JSONObject object = new JSONObject();
+		NamedOutputStream os = null;
 		OutputStreamWriter osw = null;
 		try {
 			os = getOutputStream(jcas, ".json");
@@ -30,6 +35,59 @@ public class ConfigurationHTMLExporter extends JCasFileWriter_ImplBase {
 			IOUtils.closeQuietly(osw);
 			IOUtils.closeQuietly(os);
 		}
+	}
+
+	@Override
+	public void collectionProcessComplete()
+			throws AnalysisEngineProcessException {
+		super.collectionProcessComplete();
+
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = getClass().getResourceAsStream("/html/index.html");
+			os = getOutputStream("index", ".html");
+			IOUtils.copy(is, os);
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			IOUtils.closeQuietly(os);
+			IOUtils.closeQuietly(is);
+		}
+
+		try {
+			is = getClass().getResourceAsStream("/html/exporting.js");
+			os = getOutputStream("exporting", ".js");
+			IOUtils.copy(is, os);
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			IOUtils.closeQuietly(os);
+			IOUtils.closeQuietly(is);
+		}
+
+		try {
+			is = getClass().getResourceAsStream("/html/highcharts.js");
+			os = getOutputStream("highcharts", ".js");
+			IOUtils.copy(is, os);
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			IOUtils.closeQuietly(os);
+			IOUtils.closeQuietly(is);
+		}
+
+		try {
+			is = getClass().getResourceAsStream("/html/jquery-1.11.3.min.js");
+			os = getOutputStream("jquery-1.11.3.min", ".js");
+			IOUtils.copy(is, os);
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			IOUtils.closeQuietly(os);
+			IOUtils.closeQuietly(is);
+		}
+
 	}
 
 }
