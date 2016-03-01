58b3bb0f0d4547ea9911c64b83addb037ecb5f5d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 16:31:02 +0100

    basic file writing

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
@@ -1,7 +1,13 @@
 package de.unistuttgart.quadrama.io.html;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import org.apache.commons.compress.utils.IOUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
+import org.json.JSONObject;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
 
@@ -9,7 +15,21 @@ public class ConfigurationHTMLExporter extends JCasFileWriter_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-
+		JSONObject object = null;
+		OutputStream os = null;
+		OutputStreamWriter osw = null;
+		try {
+			os = getOutputStream(jcas, ".json");
+			osw = new OutputStreamWriter(os);
+			osw.write(object.toString());
+			osw.flush();
+			osw.close();
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} finally {
+			IOUtils.closeQuietly(osw);
+			IOUtils.closeQuietly(os);
+		}
 	}
 
 }
