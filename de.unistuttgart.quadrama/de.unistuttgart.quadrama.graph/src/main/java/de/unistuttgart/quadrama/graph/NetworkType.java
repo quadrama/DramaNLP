603b5d3d6718fa477ef49236ed2e79f77053c624
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 12:21:26 +0100

    We now have a method to retrieve the class of the returned graph

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.graph/src/main/java/de/unistuttgart/quadrama/graph/NetworkType.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.graph/src/main/java/de/unistuttgart/quadrama/graph/NetworkType.java
@@ -1,5 +1,9 @@
 package de.unistuttgart.quadrama.graph;
 
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.WeightedGraph;
+
 /**
  * 
  * @author reiterns
@@ -9,5 +13,21 @@ public enum NetworkType {
 	/**
 	 * Two vertices are connected if the figures are co-present within a scene
 	 */
-	Copresence
+	Copresence,
+	/**
+	 * Directed network. Two vertices are connected, if the first figure
+	 * mentions the second.
+	 */
+	MentionNetwork;
+
+	@SuppressWarnings("rawtypes")
+	public static Class<? extends Graph> getClassForNetwork(NetworkType type) {
+		switch (type) {
+		case MentionNetwork:
+			return DirectedGraph.class;
+		default:
+			return WeightedGraph.class;
+		}
+
+	}
 }
