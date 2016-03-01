a946704b9eb0a1ed97e028a2e45cbf7587d7f936
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 12:12:58 +0100

    added network type enum

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.graph/src/main/java/de/unistuttgart/quadrama/graph/NetworkType.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.quadrama.graph;
+
+/**
+ * 
+ * @author reiterns
+ *
+ */
+public enum NetworkType {
+	/**
+	 * Two vertices are connected if the figures are co-present within a scene
+	 */
+	Copresence
+}
