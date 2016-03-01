85c0e6966a35b06272751642593a18074ecb332c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-01 16:46:53 +0100

    test files

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.html/src/test/java/de/unistuttgart/quadrama/io/html/TestHTMLExporter.java
@@ -0,0 +1,29 @@
+package de.unistuttgart.quadrama.io.html;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+
+public class TestHTMLExporter {
+	@Test
+	public void testExporter() throws ResourceInitializationException,
+	UIMAException, IOException {
+		CollectionReaderDescription crd =
+				CollectionReaderFactory.createReaderDescription(
+						XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
+						"src/test/resources/*.xmi");
+
+		SimplePipeline.runPipeline(crd, AnalysisEngineFactory
+				.createEngineDescription(ConfigurationHTMLExporter.class,
+						ConfigurationHTMLExporter.PARAM_TARGET_LOCATION,
+						"target/"));
+	}
+}
