c9c73edf3519b0a71a57ae3989f673636abf7e14
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-03-04 11:22:33 +0100

    logging while collecting

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/CollectDocuments.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.experiments/src/main/java/de/unistuttgart/quadrama/experiments/CollectDocuments.java
@@ -16,28 +16,29 @@ import de.unistuttgart.quadrama.io.tei.textgrid.TextgridTEIReader;
 public class CollectDocuments {
 
 	public static void main(String[] args) throws UIMAException, IOException {
-
+		System.setProperty("java.util.logging.config.file",
+				"src/main/resources/logging.properties");
 		System.err.println("Collecting Wieland ...");
 		SimplePipeline.runPipeline(
 				createReaderDescription(GutenbergDEReader.class,
 						GutenbergDEReader.PARAM_INPUT_DIRECTORY,
 						"src/main/resources/raw/Wieland/"),
-						createEngineDescription(SetDramaMetaData.class,
-								SetDramaMetaData.PARAM_DRAMAID, "RuJ-Wieland"),
-								createEngineDescription(XmiWriter.class,
-										XmiWriter.PARAM_TARGET_LOCATION,
-										"src/main/resources/romeo-and-juliet/"));
+				createEngineDescription(SetDramaMetaData.class,
+						SetDramaMetaData.PARAM_DRAMAID, "RuJ-Wieland"),
+				createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION,
+						"src/main/resources/romeo-and-juliet/"));
 
 		System.err.println("Collecting Schlegel ...");
 		SimplePipeline.runPipeline(
 				createReaderDescription(TextgridTEIReader.class,
 						TextgridTEIReader.PARAM_INPUT_DIRECTORY,
 						"src/main/resources/raw/Schlegel/"),
-						createEngineDescription(SetDramaMetaData.class,
-								SetDramaMetaData.PARAM_DRAMAID, "RuJ-Schlegel"),
-								createEngineDescription(XmiWriter.class,
-										XmiWriter.PARAM_TARGET_LOCATION,
-						"src/main/resources/romeo-and-juliet/"));
+				createEngineDescription(SetDramaMetaData.class,
+						SetDramaMetaData.PARAM_DRAMAID, "RuJ-Schlegel"),
+				createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION,
+										"src/main/resources/romeo-and-juliet/"));
 
 		System.err.println("Collecting Shakespeare ...");
 		SimplePipeline.runPipeline(
@@ -45,11 +46,11 @@ public class CollectDocuments {
 						TextgridTEIReader.PARAM_INPUT_DIRECTORY,
 						"src/main/resources/raw/Shakespeare/",
 						TextgridTEIReader.PARAM_LANGUAGE, "en"),
-						createEngineDescription(SetDramaMetaData.class,
-								SetDramaMetaData.PARAM_DRAMAID, "RuJ-Shakespeare"),
-								createEngineDescription(XmiWriter.class,
-										XmiWriter.PARAM_TARGET_LOCATION,
-						"src/main/resources/romeo-and-juliet/"));
+				createEngineDescription(SetDramaMetaData.class,
+						SetDramaMetaData.PARAM_DRAMAID, "RuJ-Shakespeare"),
+				createEngineDescription(XmiWriter.class,
+						XmiWriter.PARAM_TARGET_LOCATION,
+										"src/main/resources/romeo-and-juliet/"));
 	}
 
 }
