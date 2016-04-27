c151b4509c798a4e969edc301bc0256e6ad1abae
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-04-27 13:28:22 +0200

    removed debug output

--- de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.core/src/main/java/de/unistuttgart/quadrama/io/core/AbstractDramaUrlReader.java
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.io.core/src/main/java/de/unistuttgart/quadrama/io/core/AbstractDramaUrlReader.java
@@ -1,11 +1,13 @@
 package de.unistuttgart.quadrama.io.core;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.uima.UimaContext;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
@@ -18,7 +20,7 @@ import org.apache.uima.util.Progress;
 import de.unistuttgart.quadrama.api.Drama;
 
 public abstract class AbstractDramaUrlReader extends
-JCasCollectionReader_ImplBase {
+		JCasCollectionReader_ImplBase {
 	public static final String PARAM_URL_LIST = "URL List";
 	public static final String PARAM_LANGUAGE = "Language";
 
@@ -29,9 +31,7 @@ JCasCollectionReader_ImplBase {
 			defaultValue = "de")
 	String language = "de";
 
-	List<String> urls =
-			Arrays.asList("https://textgridlab.org/1.0/tgcrud-public/rest/textgrid:t4rs.0/data");
-
+	List<String> urls = null;
 	int currentUrlIndex = 0;
 
 	@Override
@@ -40,9 +40,9 @@ JCasCollectionReader_ImplBase {
 		super.initialize(context);
 
 		try {
-			// urls =
-			// IOUtils.readLines(new FileInputStream(new File(
-			// urlListFilename)));
+			urls =
+					IOUtils.readLines(new FileInputStream(new File(
+							urlListFilename)));
 			getLogger().log(Level.FINE, "Found " + urls.size() + " URLs.");
 		} catch (Exception e) {
 			throw new ResourceInitializationException(e);
@@ -51,8 +51,6 @@ JCasCollectionReader_ImplBase {
 	}
 
 	public boolean hasNext() throws IOException, CollectionException {
-		System.err.println("checking: " + currentUrlIndex + " with "
-				+ urls.size() + ": " + (currentUrlIndex < urls.size()));
 		return currentUrlIndex < urls.size();
 	}
 
@@ -69,7 +67,7 @@ JCasCollectionReader_ImplBase {
 
 		Drama drama = new Drama(jcas);
 		drama.setDocumentId(String.valueOf(currentUrlIndex));
-		drama.setDocumentBaseUri(currentUrl);
+		drama.setDocumentBaseUri("https://textgridlab.org/1.0/tgcrud-public/rest/");
 		drama.setDocumentUri(currentUrl);
 		drama.addToIndexes();
 		jcas.setDocumentLanguage(language);
