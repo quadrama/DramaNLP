aa687aa73de447df7874c90b97a3521363f264f7
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-02 15:54:59 +0200

    code and resources for evaluating the coverage of the speaker identifier

---
+++ de.unistuttgart.quadrama/de.unistuttgart.quadrama.core/src/test/java/de/unistuttgart/quadrama/core/TestSpeakerIdentifier.java
@@ -0,0 +1,68 @@
+package de.unistuttgart.quadrama.core;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.JCasIterator;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
+import de.unistuttgart.ims.commons.Counter;
+import de.unistuttgart.quadrama.api.Drama;
+import de.unistuttgart.quadrama.api.Speaker;
+
+public class TestSpeakerIdentifier {
+
+	@Test
+	public void evaluateSpeakerAssignments() throws Exception {
+		JCasIterator iter =
+				SimplePipeline
+						.iteratePipeline(
+								CollectionReaderFactory
+										.createReaderDescription(
+												XmiReader.class,
+												XmiReader.PARAM_SOURCE_LOCATION,
+												"src/test/resources/SpeakerIdentifier/tx4z.0.xmi"),
+								createEngineDescription(FigureReferenceAnnotator.class),
+								createEngineDescription(
+										SpeakerIdentifier.class,
+										SpeakerIdentifier.PARAM_CREATE_SPEAKER_FIGURE,
+										false),
+								createEngineDescription(XmiWriter.class,
+										XmiWriter.PARAM_TARGET_LOCATION,
+										"target/doc")).iterator();
+		StringBuilder b = new StringBuilder();
+		CSVPrinter writer = new CSVPrinter(b, CSVFormat.TDF);
+		SummaryStatistics types = new SummaryStatistics();
+		SummaryStatistics tokens = new SummaryStatistics();
+		while (iter.hasNext()) {
+			JCas jcas = iter.next();
+			int s = 0;
+			int all = 0;
+			Counter<String> unassigned = new Counter<String>();
+			for (Speaker speaker : JCasUtil.select(jcas, Speaker.class)) {
+				if (speaker.getFigure() == null) {
+					unassigned.add(speaker.getCoveredText());
+					s++;
+				}
+				all++;
+			}
+			types.addValue(unassigned.size());
+			tokens.addValue(s);
+			writer.printRecord(JCasUtil.selectSingle(jcas, Drama.class)
+					.getDocumentId(), s, unassigned.size(), all);
+		}
+		writer.printRecord("mean", tokens.getMean(), types.getMean());
+		writer.printRecord("min", tokens.getMin(), types.getMin());
+		writer.printRecord("max", tokens.getMax(), types.getMax());
+		writer.close();
+		System.out.println(b.toString());
+	}
+}
