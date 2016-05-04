60272a2e97c60261a4e578c54a0f04d0d608d0ee
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-04 07:51:48 +0200

    now using AbstractDramaUrlReader as base class

--- de.unistuttgart.ims.drama.io.gutenbergde/src/main/java/de/unistuttgart/quadrama/io/gutenbergde/GutenbergDEReader.java
+++ de.unistuttgart.ims.drama.io.gutenbergde/src/main/java/de/unistuttgart/quadrama/io/gutenbergde/GutenbergDEReader.java
@@ -36,16 +36,15 @@ import de.unistuttgart.ims.drama.api.Speech;
 import de.unistuttgart.ims.drama.api.StageDirection;
 import de.unistuttgart.ims.drama.api.Utterance;
 import de.unistuttgart.ims.uimautil.AnnotationUtil;
-import de.unistuttgart.quadrama.io.core.AbstractDramaFileReader;
+import de.unistuttgart.quadrama.io.core.AbstractDramaUrlReader;
 import de.unistuttgart.quadrama.io.core.DramaIOUtil;
 import de.unistuttgart.quadrama.io.core.Visitor;
 import de.unistuttgart.quadrama.io.core.type.HTMLAnnotation;
 
-public class GutenbergDEReader extends AbstractDramaFileReader {
+public class GutenbergDEReader extends AbstractDramaUrlReader {
 
 	@Override
-	public void getNext(JCas jcas, InputStream file, Drama drama)
-			throws IOException, CollectionException {
+	public void getNext(JCas jcas, InputStream file, Drama drama) throws IOException, CollectionException {
 
 		String str = IOUtils.toString(file);
 		org.jsoup.nodes.Document doc = Jsoup.parseBodyFragment(str);
@@ -55,37 +54,27 @@ public class GutenbergDEReader extends AbstractDramaFileReader {
 		Map<String, HTMLAnnotation> annoMap = vis.getAnnotationMap();
 
 		// identify front and main matter
-		select2Annotation(jcas, doc, annoMap, "div.gutenb:eq(0)",
-				FrontMatter.class, null);
-		selectRange2Annotation(jcas, doc, annoMap, "div.gutenb:eq(1)",
-				"div.gutenb:last-child", MainMatter.class);
-		FrontMatter frontMatter =
-				JCasUtil.selectSingle(jcas, FrontMatter.class);
+		select2Annotation(jcas, doc, annoMap, "div.gutenb:eq(0)", FrontMatter.class, null);
+		selectRange2Annotation(jcas, doc, annoMap, "div.gutenb:eq(1)", "div.gutenb:last-child", MainMatter.class);
+		FrontMatter frontMatter = JCasUtil.selectSingle(jcas, FrontMatter.class);
 		MainMatter mainMatter = JCasUtil.selectSingle(jcas, MainMatter.class);
 
 		// identify simple annotations
-		select2Annotation(jcas, doc, annoMap, "span.speaker", Speaker.class,
-				mainMatter);
-		select2Annotation(jcas, doc, annoMap, "span.speaker", Figure.class,
-				frontMatter);
-		select2Annotation(jcas, doc, annoMap, "span.regie",
-				StageDirection.class, mainMatter);
-		select2Annotation(jcas, doc, annoMap, "span.footnote", Footnote.class,
-				mainMatter);
-		select2Annotation(jcas, doc, annoMap, "h3 + p", DramatisPersonae.class,
-				frontMatter);
+		select2Annotation(jcas, doc, annoMap, "span.speaker", Speaker.class, mainMatter);
+		select2Annotation(jcas, doc, annoMap, "span.speaker", Figure.class, frontMatter);
+		select2Annotation(jcas, doc, annoMap, "span.regie", StageDirection.class, mainMatter);
+		select2Annotation(jcas, doc, annoMap, "span.footnote", Footnote.class, mainMatter);
+		select2Annotation(jcas, doc, annoMap, "h3 + p", DramatisPersonae.class, frontMatter);
 
 		// find utterances
-		select2Annotation(jcas, doc, annoMap, "p:has(span.speaker)",
-				Utterance.class, mainMatter);
+		select2Annotation(jcas, doc, annoMap, "p:has(span.speaker)", Utterance.class, mainMatter);
 
 		// some utterances continue in the next paragraph
 		// they are (in RuJ) marked with class leftmarg
 		Elements elms = doc.select("p.leftmarg");
 		for (Element elm : elms) {
 			HTMLAnnotation hAnno = annoMap.get(elm.cssSelector());
-			Utterance utterance =
-					JCasUtil.selectPreceding(Utterance.class, hAnno, 1).get(0);
+			Utterance utterance = JCasUtil.selectPreceding(Utterance.class, hAnno, 1).get(0);
 			utterance.setEnd(hAnno.getEnd());
 		}
 
@@ -97,72 +86,59 @@ public class GutenbergDEReader extends AbstractDramaFileReader {
 		int currentActBegin = -1;
 		for (HTMLAnnotation anno : JCasUtil.select(jcas, HTMLAnnotation.class)) {
 			if (anno.getTag().equals("h2") && !anno.getCls().contains("author")) {
-				AnnotationFactory.createAnnotation(jcas, anno.getBegin(),
-						anno.getEnd(), SceneHeading.class);
+				AnnotationFactory.createAnnotation(jcas, anno.getBegin(), anno.getEnd(), SceneHeading.class);
 				if (currentSceneBegin >= 0) {
-					AnnotationFactory.createAnnotation(jcas, currentSceneBegin,
-							anno.getBegin() - 1, Scene.class);
+					AnnotationFactory.createAnnotation(jcas, currentSceneBegin, anno.getBegin() - 1, Scene.class);
 				}
 				currentSceneBegin = anno.getBegin();
 			}
 			if (anno.getTag().equals("h1") && !anno.getCls().contains("title")) {
-				AnnotationFactory.createAnnotation(jcas, anno.getBegin(),
-						anno.getEnd(), ActHeading.class);
+				AnnotationFactory.createAnnotation(jcas, anno.getBegin(), anno.getEnd(), ActHeading.class);
 				if (currentActBegin >= 0) {
-					AnnotationFactory.createAnnotation(jcas, currentActBegin,
-							anno.getBegin() - 1, Act.class);
+					AnnotationFactory.createAnnotation(jcas, currentActBegin, anno.getBegin() - 1, Act.class);
 				}
 				currentActBegin = anno.getBegin();
 			}
 		}
 		if (currentActBegin >= 0) {
-			AnnotationFactory.createAnnotation(jcas, currentActBegin,
-					mainMatter.getEnd(), Act.class);
+			AnnotationFactory.createAnnotation(jcas, currentActBegin, mainMatter.getEnd(), Act.class);
 		}
 		if (currentSceneBegin >= 0) {
-			AnnotationFactory.createAnnotation(jcas, currentSceneBegin,
-					mainMatter.getEnd(), Scene.class);
+			AnnotationFactory.createAnnotation(jcas, currentSceneBegin, mainMatter.getEnd(), Scene.class);
 		}
-		AnnotationUtil.trim(new ArrayList<Scene>(JCasUtil.select(jcas,
-				Scene.class)));
-		AnnotationUtil
-		.trim(new ArrayList<Act>(JCasUtil.select(jcas, Act.class)));
+		AnnotationUtil.trim(new ArrayList<Scene>(JCasUtil.select(jcas, Scene.class)));
+		AnnotationUtil.trim(new ArrayList<Act>(JCasUtil.select(jcas, Act.class)));
 
 		DramaIOUtil.cleanUp(jcas);
 
 	}
 
 	protected void annotateSpeech(JCas jcas, Annotation mainMatter) {
-		for (Utterance utterance : JCasUtil.selectCovered(Utterance.class,
-				mainMatter)) {
-			TreeSet<Annotation> except =
-					new TreeSet<Annotation>(new Comparator<Annotation>() {
+		for (Utterance utterance : JCasUtil.selectCovered(Utterance.class, mainMatter)) {
+			TreeSet<Annotation> except = new TreeSet<Annotation>(new Comparator<Annotation>() {
 
-						public int compare(Annotation o1, Annotation o2) {
-							return Integer.compare(o1.getBegin(), o2.getBegin());
-						}
+				@Override
+				public int compare(Annotation o1, Annotation o2) {
+					return Integer.compare(o1.getBegin(), o2.getBegin());
+				}
 
-					});
-			except.addAll(JCasUtil.selectCovered(StageDirection.class,
-					utterance));
+			});
+			except.addAll(JCasUtil.selectCovered(StageDirection.class, utterance));
 			except.addAll(JCasUtil.selectCovered(Speaker.class, utterance));
 			except.addAll(JCasUtil.selectCovered(Footnote.class, utterance));
 			int b = utterance.getBegin();
 			for (Annotation exc : except) {
 				if (exc.getBegin() > b) {
-					AnnotationUtil.trim(AnnotationFactory.createAnnotation(
-							jcas, b, exc.getBegin(), Speech.class));
+					AnnotationUtil.trim(AnnotationFactory.createAnnotation(jcas, b, exc.getBegin(), Speech.class));
 				}
 				b = exc.getEnd();
 			}
 			if (b < utterance.getEnd()) {
-				AnnotationUtil.trim(AnnotationFactory.createAnnotation(jcas, b,
-						utterance.getEnd(), Speech.class));
+				AnnotationUtil.trim(AnnotationFactory.createAnnotation(jcas, b, utterance.getEnd(), Speech.class));
 			}
 		}
 
-		for (Speech speech : new ArrayList<Speech>(JCasUtil.select(jcas,
-				Speech.class))) {
+		for (Speech speech : new ArrayList<Speech>(JCasUtil.select(jcas, Speech.class))) {
 			if (speech.getCoveredText().matches("^\\s*$")) {
 				speech.removeFromIndexes();
 			} else
@@ -175,21 +151,20 @@ public class GutenbergDEReader extends AbstractDramaFileReader {
 	}
 
 	protected void assignSpeakerIds(JCas jcas) {
-		DramatisPersonae dp =
-				JCasUtil.selectSingle(jcas, DramatisPersonae.class);
+		DramatisPersonae dp = JCasUtil.selectSingle(jcas, DramatisPersonae.class);
 
 		int speakerId = 1;
 		Map<String, Speaker> speakerMap = new HashMap<String, Speaker>();
 		for (Speaker speaker : JCasUtil.selectCovered(Speaker.class, dp)) {
 			speaker.setId(speakerId++);
 			speakerMap.put(speaker.getCoveredText(), speaker);
-		};
+		}
+		;
 
 		for (Speaker speaker : JCasUtil.select(jcas, Speaker.class)) {
 			if (speaker.getId() == 0) {
 				try {
-					speaker.setId(speakerMap.get(speaker.getCoveredText())
-							.getId());
+					speaker.setId(speakerMap.get(speaker.getCoveredText()).getId());
 				} catch (NullPointerException e) {
 					// no entry in speaker map
 				}
