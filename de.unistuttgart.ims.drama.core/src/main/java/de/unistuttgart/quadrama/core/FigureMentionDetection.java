d9cd19ecc3424db0b939d3337ddd5ac0119cc1c4
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-06 10:28:52 +0200

    Now we also resolve first person pronouns

--- de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/FigureMentionDetection.java
+++ de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/FigureMentionDetection.java
@@ -1,34 +1,81 @@
 package de.unistuttgart.quadrama.core;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.PR;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.unistuttgart.ims.drama.api.Figure;
 import de.unistuttgart.ims.drama.api.FigureMention;
+import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Speech;
+import de.unistuttgart.ims.drama.api.Utterance;
 
 public class FigureMentionDetection extends JCasAnnotator_ImplBase {
 
+	Map<String, List<String>> firstPersonPronouns = new HashMap<String, List<String>>();
+
+	@Override
+	public void initialize(final UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+
+		try {
+			firstPersonPronouns.put("de",
+					IOUtils.readLines(getClass().getResourceAsStream("/pronouns/de/person1.csv")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		for (Speech speech : JCasUtil.select(jcas, Speech.class)) {
-			for (Figure figure : JCasUtil.select(jcas, Figure.class)) {
+
+		Map<String, Figure> figureMap = new HashMap<String, Figure>();
+		for (Figure figure : JCasUtil.select(jcas, Figure.class)) {
+			figureMap.put(figure.getCoveredText(), figure);
+			figureMap.put(figure.getReference(), figure);
+		}
+		List<String> pronouns = firstPersonPronouns.get(jcas.getDocumentLanguage());
+
+		for (Utterance utterance : JCasUtil.select(jcas, Utterance.class)) {
+			Figure currentFigure = null;
+			try {
+				currentFigure = JCasUtil.selectCovered(jcas, Speaker.class, utterance).get(0).getFigure();
+			} catch (NullPointerException e) {
+
+			}
+			for (Speech speech : JCasUtil.selectCovered(jcas, Speech.class, utterance)) {
 				for (Token token : JCasUtil.selectCovered(Token.class, speech)) {
-					if (token.getCoveredText().equalsIgnoreCase(
-							figure.getCoveredText())) {
-						FigureMention fm =
-								AnnotationFactory.createAnnotation(jcas,
-										token.getBegin(), token.getEnd(),
-										FigureMention.class);
-						fm.setFigure(figure);
+					String surface = token.getCoveredText();
+					if (figureMap.containsKey(surface)) {
+						FigureMention fm = AnnotationFactory.createAnnotation(jcas, token.getBegin(), token.getEnd(),
+								FigureMention.class);
+						fm.setFigure(figureMap.get(surface));
 					}
 				}
+				if (currentFigure != null)
+					for (PR pronoun : JCasUtil.selectCovered(jcas, PR.class, speech)) {
+						if (pronouns.contains(pronoun.getCoveredText())) {
+							AnnotationFactory
+									.createAnnotation(jcas, pronoun.getBegin(), pronoun.getEnd(), FigureMention.class)
+									.setFigure(currentFigure);
+						}
+					}
 			}
 		}
+
 	}
 
 }
