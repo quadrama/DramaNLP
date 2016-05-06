b3080aa2455b499d15731c9d30b8a7b40a03a1f2
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-06 16:15:34 +0200

    added assertions

--- de.unistuttgart.ims.drama.graph/src/test/java/de/unistuttgart/quadrama/graph/ext/TestGraphIO.java
+++ de.unistuttgart.ims.drama.graph/src/test/java/de/unistuttgart/quadrama/graph/ext/TestGraphIO.java
@@ -1,6 +1,8 @@
 package de.unistuttgart.quadrama.graph.ext;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -36,9 +38,7 @@ public class TestGraphIO {
 		jcas.setDocumentLanguage("de");
 
 		for (int i = 0; i < figures.length; i++) {
-			figures[i] =
-					AnnotationFactory.createAnnotation(jcas, i * 2, i * 2 + 1,
-							Figure.class);
+			figures[i] = AnnotationFactory.createAnnotation(jcas, i * 2, i * 2 + 1, Figure.class);
 			figures[i].setId(i);
 		}
 
@@ -46,36 +46,32 @@ public class TestGraphIO {
 	}
 
 	@Test
-	public void testGraphIO() throws IOException, CASException,
-			ClassNotFoundException, InstantiationException,
-			IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, NoSuchMethodException, SecurityException {
-		WeightedGraph<Figure, DefaultWeightedEdge> graph =
-				new SimpleWeightedGraph<Figure, DefaultWeightedEdge>(
-						DefaultWeightedEdge.class);
+	public void testGraphIO()
+			throws IOException, CASException, ClassNotFoundException, InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		WeightedGraph<Figure, DefaultWeightedEdge> graph = new SimpleWeightedGraph<Figure, DefaultWeightedEdge>(
+				DefaultWeightedEdge.class);
 		for (int i = 0; i < figures.length; i++) {
 			graph.addVertex(figures[i]);
 		}
 		for (int i = 0; i < figures.length - 1; i++) {
-			DefaultWeightedEdge edge =
-					graph.addEdge(figures[i], figures[i + 1]);
+			DefaultWeightedEdge edge = graph.addEdge(figures[i], figures[i + 1]);
 			graph.setEdgeWeight(edge, random.nextDouble());
 		}
 
 		GraphExporter ge = new GraphExporter();
 		ge.export(jcas.createView("Testview"), graph);
 
-		Graph<Figure, DefaultWeightedEdge> g2 =
-				GraphImporter.getGraph(jcas, "Testview");
+		Graph<Figure, DefaultWeightedEdge> g2 = GraphImporter.getGraph(jcas, "Testview");
 
+		assertNotNull(g2);
+		assertFalse(g2.vertexSet().isEmpty());
 		for (int i = 0; i < figures.length; i++) {
 			for (int j = 0; j < figures.length; j++) {
-				assertEquals(graph.containsEdge(figures[i], figures[j]),
-						g2.containsEdge(figures[i], figures[j]));
+				assertEquals(graph.containsEdge(figures[i], figures[j]), g2.containsEdge(figures[i], figures[j]));
 				if (graph.containsEdge(figures[i], figures[j])) {
-					assertEquals(graph.getEdgeWeight(graph.getEdge(figures[i],
-							figures[j])), g2.getEdgeWeight(g2.getEdge(
-							figures[i], figures[j])), 1e-3);
+					assertEquals(graph.getEdgeWeight(graph.getEdge(figures[i], figures[j])),
+							g2.getEdgeWeight(g2.getEdge(figures[i], figures[j])), 1e-3);
 				}
 			}
 		}
