d01eba47530ccde05df6a861d79823f1c02ec48e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-11 14:56:09 +0200

    more metadata for a drama

--- de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/SetDramaMetaData.java
+++ de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/SetDramaMetaData.java
@@ -10,23 +10,54 @@ import de.unistuttgart.ims.drama.api.Drama;
 
 public class SetDramaMetaData extends JCasAnnotator_ImplBase {
 
-	public static final String PARAM_AUTHOR = "Author";
+	public static final String PARAM_AUTHOR_NAME = "Author Name";
+	public static final String PARAM_AUTHOR_PND = "Author PND";
 	public static final String PARAM_DRAMAID = "Drama Id";
+	public static final String PARAM_REFERENCEDATE = "Reference Date";
 
-	@ConfigurationParameter(name = PARAM_AUTHOR, mandatory = false)
+	public static final String PARAM_TRANSLATION = "Translation";
+	public static final String PARAM_TRANSLATOR_NAME = "Translator Name";
+	public static final String PARAM_TRANSLATOR_PND = "Translator PND";
+
+	@ConfigurationParameter(name = PARAM_AUTHOR_NAME, mandatory = false)
 	String authorName = null;
+
+	@ConfigurationParameter(name = PARAM_AUTHOR_PND, mandatory = false)
+	String authorPnd = null;
 
 	@ConfigurationParameter(name = PARAM_DRAMAID, mandatory = false)
 	String dramaId = null;
+
+	@ConfigurationParameter(name = PARAM_REFERENCEDATE, mandatory = false)
+	int referenceDate = -1;
+
+	@ConfigurationParameter(name = PARAM_TRANSLATION, mandatory = false)
+	boolean translation = false;
+
+	@ConfigurationParameter(name = PARAM_TRANSLATOR_NAME, mandatory = false)
+	String translatorName = null;
+
+	@ConfigurationParameter(name = PARAM_TRANSLATOR_PND, mandatory = false)
+	String translatorPnd = null;
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		Drama drama = JCasUtil.selectSingle(jcas, Drama.class);
 
-		if (authorName != null) drama.setAuthorname(authorName);
-		if (dramaId != null) {
-			drama.setDramaId(dramaId);
+		if (authorName != null)
+			drama.setAuthorname(authorName);
+		if (authorPnd != null)
+			drama.setAuthorPnd(authorPnd);
+		if (dramaId != null)
 			drama.setDocumentId(dramaId);
-		}
+		if (referenceDate > 0)
+			drama.setReferenceDate(referenceDate);
+
+		drama.setTranslation(translation);
+		if (translatorName != null)
+			drama.setTranslatorName(translatorName);
+		if (translatorPnd != null)
+			drama.setTranslatorPnd(translatorPnd);
+
 	}
 }
