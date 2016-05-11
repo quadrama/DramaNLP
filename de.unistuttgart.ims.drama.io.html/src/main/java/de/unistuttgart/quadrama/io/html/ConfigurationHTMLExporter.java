f3b0ec0ffb03182f93b408b28ea1a62b63557193
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-11 09:52:51 +0200

    we now print both ids in the title, also setting utf8 as charset

--- de.unistuttgart.ims.drama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
+++ de.unistuttgart.ims.drama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/ConfigurationHTMLExporter.java
@@ -119,7 +119,8 @@ public class ConfigurationHTMLExporter extends JCasFileWriter_ImplBase {
 		}
 
 		obj.put("data", jsonSeries);
-		obj.put("id", documentId);
+		obj.put("displayId", documentId);
+		obj.put("id", drama.getDocumentId());
 		obj.put("author", drama.getAuthorname());
 		obj.put("title", drama.getDocumentTitle());
 
