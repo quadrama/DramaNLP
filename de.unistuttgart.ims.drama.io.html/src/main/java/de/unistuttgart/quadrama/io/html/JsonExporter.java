20ec40f670688c20cbcc808aefa07d89c59e7a89
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-11 09:46:28 +0200

    now we export acts and scenes

--- de.unistuttgart.ims.drama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/JsonExporter.java
+++ de.unistuttgart.ims.drama.io.html/src/main/java/de/unistuttgart/quadrama/io/html/JsonExporter.java
@@ -3,6 +3,7 @@ package de.unistuttgart.quadrama.io.html;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.List;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -14,8 +15,12 @@ import org.apache.uima.jcas.tcas.Annotation;
 import org.json.JSONObject;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+import de.unistuttgart.ims.drama.api.Act;
+import de.unistuttgart.ims.drama.api.ActHeading;
 import de.unistuttgart.ims.drama.api.Drama;
 import de.unistuttgart.ims.drama.api.Figure;
+import de.unistuttgart.ims.drama.api.Scene;
+import de.unistuttgart.ims.drama.api.SceneHeading;
 
 public class JsonExporter extends JCasFileWriter_ImplBase {
 
@@ -41,7 +46,25 @@ public class JsonExporter extends JCasFileWriter_ImplBase {
 
 		// figures
 		for (Figure figure : JCasUtil.select(aJCas, Figure.class)) {
-			json.append("figures", convert(figure));
+			json.append("figures", convert(figure, true));
+		}
+
+		// segments
+		for (Act act : JCasUtil.select(aJCas, Act.class)) {
+			List<ActHeading> ahl = JCasUtil.selectCovered(ActHeading.class, act);
+			if (ahl.isEmpty()) {
+				json.append("acts", convert(act, false));
+			} else {
+				json.append("acts", convert(act, false).put("heading", ahl.get(0).getCoveredText()));
+			}
+			for (Scene scene : JCasUtil.selectCovered(Scene.class, act)) {
+				List<SceneHeading> shl = JCasUtil.selectCovered(SceneHeading.class, scene);
+				if (shl.isEmpty()) {
+					json.append("scenes", convert(scene, false));
+				} else {
+					json.append("scenes", convert(scene, false).put("heading", shl.get(0).getCoveredText()));
+				}
+			}
 		}
 
 		// assembly
@@ -62,9 +85,10 @@ public class JsonExporter extends JCasFileWriter_ImplBase {
 
 	}
 
-	public static <T extends Annotation> JSONObject convert(T annotation) {
+	public static <T extends Annotation> JSONObject convert(T annotation, boolean includeText) {
 		JSONObject object = new JSONObject();
-		object.put("coveredText", annotation.getCoveredText());
+		if (includeText)
+			object.put("coveredText", annotation.getCoveredText());
 		for (Feature feature : annotation.getType().getFeatures()) {
 			if (feature.getRange().isPrimitive()) {
 				object.put(feature.getShortName(), annotation.getFeatureValueAsString(feature));
