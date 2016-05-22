83bf5db7b94236df0ef2a04d1a0809a58eaa869e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-22 10:28:55 +0200

    added typecapability annotation

--- de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/FigureReferenceAnnotator.java
+++ de.unistuttgart.ims.drama.core/src/main/java/de/unistuttgart/quadrama/core/FigureReferenceAnnotator.java
@@ -7,11 +7,14 @@ import java.util.regex.Pattern;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.TypeCapability;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
 import de.unistuttgart.ims.drama.api.Figure;
 
+@TypeCapability(inputs = { "de.unistuttgart.ims.drama.api.Figure" }, outputs = {
+		"de.unistuttgart.ims.drama.api.Figure:Reference" })
 public class FigureReferenceAnnotator extends JCasAnnotator_ImplBase {
 
 	Pattern pattern = Pattern.compile("\\p{Punct}", 0);
