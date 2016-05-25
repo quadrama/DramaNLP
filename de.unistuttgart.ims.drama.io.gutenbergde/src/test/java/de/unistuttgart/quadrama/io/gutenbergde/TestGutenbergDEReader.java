3daabb8b97f07834f7c9dbf171240cb5ddd74f89
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-05-25 08:27:31 +0200

    code to test and reproduce #8.

--- de.unistuttgart.ims.drama.io.gutenbergde/src/test/java/de/unistuttgart/quadrama/io/gutenbergde/TestGutenbergDEReader.java
+++ de.unistuttgart.ims.drama.io.gutenbergde/src/test/java/de/unistuttgart/quadrama/io/gutenbergde/TestGutenbergDEReader.java
@@ -45,6 +45,7 @@ public class TestGutenbergDEReader {
 		JCasIterator iter = SimplePipeline.iteratePipeline(description, AnalysisEngineFactory
 				.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_TARGET_LOCATION, "target/doc")).iterator();
 		JCas jcas;
+		Scene scene;
 
 		jcas = iter.next();
 		// sanity check
@@ -60,8 +61,13 @@ public class TestGutenbergDEReader {
 		assertEquals(5, JCasUtil.select(jcas, Act.class).size());
 		assertEquals(31, JCasUtil.select(jcas, Scene.class).size());
 
+		// 6th scene
+		scene = JCasUtil.selectByIndex(jcas, Scene.class, 5);
+		assertEquals("oppeln.]", scene.getCoveredText().substring(scene.getCoveredText().length() - 8));
+		assertTrue(scene.getCoveredText().endsWith("verdoppeln.]"));
+
 		// Balkony scene
-		Scene scene = JCasUtil.selectByIndex(jcas, Scene.class, 7);
+		scene = JCasUtil.selectByIndex(jcas, Scene.class, 7);
 		Iterator<Utterance> utteranceIter = JCasUtil.selectCovered(Utterance.class, scene).iterator();
 		Utterance utter;
 		utter = utteranceIter.next();
