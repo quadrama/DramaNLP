c57994ebd4b945d58758043047b75572cf92c1e6
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-11-18 02:26:30 +0100

    additional dependencies, Training file

--- de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/gender/Training.java
+++ de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/gender/Training.java
@@ -1,7 +1,9 @@
 package de.unistuttgart.ims.drama.core.ml.gender;
 
 import java.io.File;
+import java.util.Arrays;
 
+import org.apache.uima.cas.CAS;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
@@ -10,9 +12,13 @@ import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.cleartk.ml.jar.DefaultDataWriterFactory;
 import org.cleartk.ml.jar.DirectoryDataWriterFactory;
 import org.cleartk.ml.jar.Train;
-import org.cleartk.ml.libsvm.LibSvmStringOutcomeDataWriter;
+import org.cleartk.ml.weka.WekaStringOutcomeDataWriter;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.unistuttgart.ims.drama.api.DramatisPersonae;
+import de.unistuttgart.ims.drama.api.Figure;
+import de.unistuttgart.ims.drama.core.ml.PrepareClearTk;
 
 public class Training {
 
@@ -21,12 +27,19 @@ public class Training {
 				XmiReader.PARAM_SOURCE_LOCATION, "src/main/resources/gender/training/*.xmi", XmiReader.PARAM_LENIENT,
 				true);
 
+		String tmpView = "DP";
+
 		AggregateBuilder b = new AggregateBuilder();
 
+		b.add(AnalysisEngineFactory.createEngineDescription(PrepareClearTk.class, PrepareClearTk.PARAM_VIEW_NAME,
+				tmpView, PrepareClearTk.PARAM_ANNOTATION_TYPE, DramatisPersonae.class,
+				PrepareClearTk.PARAM_SUBANNOTATIONS, Arrays.asList(Figure.class.getName())), CAS.NAME_DEFAULT_SOFA,
+				tmpView);
+		b.add(AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class), CAS.NAME_DEFAULT_SOFA,
+				tmpView);
 		b.add(AnalysisEngineFactory.createEngineDescription(ClearTkGenderAnnotator.class,
-				DefaultDataWriterFactory.PARAM_DATA_WRITER_CLASS_NAME, LibSvmStringOutcomeDataWriter.class,
-				DirectoryDataWriterFactory.PARAM_OUTPUT_DIRECTORY, "target/models"));
-
+				DefaultDataWriterFactory.PARAM_DATA_WRITER_CLASS_NAME, WekaStringOutcomeDataWriter.class,
+				DirectoryDataWriterFactory.PARAM_OUTPUT_DIRECTORY, "target/models"), CAS.NAME_DEFAULT_SOFA, tmpView);
 		SimplePipeline.runPipeline(reader, b.createAggregateDescription());
 
 		Train.main(new File("target/models"), new String[] { "-t", "0" });
