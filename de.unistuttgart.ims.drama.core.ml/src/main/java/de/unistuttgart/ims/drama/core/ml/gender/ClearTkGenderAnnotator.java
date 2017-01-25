e4b6e287dac572d1cc7cc9b6d3145097121a3fe5
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-01-25 07:49:31 +0100

    Mapping back only a single feature

--- de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/gender/ClearTkGenderAnnotator.java
+++ de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/gender/ClearTkGenderAnnotator.java
@@ -30,7 +30,7 @@ import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unistuttgart.ims.drama.api.DramatisPersonae;
 import de.unistuttgart.ims.drama.api.Figure;
 import de.unistuttgart.ims.drama.api.FigureType;
-import de.unistuttgart.ims.drama.core.ml.MapBackAnnotations;
+import de.unistuttgart.ims.drama.core.ml.MapBackFeature;
 import de.unistuttgart.ims.drama.core.ml.PrepareClearTk;
 
 public class ClearTkGenderAnnotator extends CleartkAnnotator<String> {
@@ -135,8 +135,9 @@ public class ClearTkGenderAnnotator extends CleartkAnnotator<String> {
 				BreakIteratorSegmenter.PARAM_WRITE_SENTENCE, false), CAS.NAME_DEFAULT_SOFA, tmpView);
 		b.add(AnalysisEngineFactory.createEngineDescription(ClearTkGenderAnnotator.class,
 				GenericJarClassifierFactory.PARAM_CLASSIFIER_JAR_PATH, genderModelUrl), CAS.NAME_DEFAULT_SOFA, tmpView);
-		b.add(AnalysisEngineFactory.createEngineDescription(MapBackAnnotations.class, MapBackAnnotations.PARAM_ANNOTATION_TYPE,
-				FigureType.class, MapBackAnnotations.PARAM_VIEW_NAME, tmpView));
+		b.add(AnalysisEngineFactory.createEngineDescription(MapBackFeature.class, MapBackFeature.PARAM_ANNOTATION_TYPE,
+				FigureType.class, MapBackFeature.PARAM_VIEW_NAME, tmpView, MapBackFeature.PARAM_FEATURE_NAME,
+				"Gender"));
 
 		return b.createAggregateDescription();
 	}
