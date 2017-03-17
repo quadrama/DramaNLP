14aab5425be9a8733a8314f8372a4946402f2cb9
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-03-17 13:40:18 +0100

    Updated vocabulary

--- de.unistuttgart.ims.drama.meta/src/main/java/de/unistuttgart/ims/drama/meta/vocabulary/QD.java
+++ de.unistuttgart.ims.drama.meta/src/main/java/de/unistuttgart/ims/drama/meta/vocabulary/QD.java
@@ -39,16 +39,29 @@ public class QD {
 	public static final Resource NAMESPACE = m_model.createResource(NS);
 
 	public static final Resource Premiere = m_model.createResource(NS + "Premiere");
+	public static final Resource DramaticFigure = m_model.createResource(NS + "DramaticFigure");
+	public static final Resource DramaticDifferentiatedFigure = m_model
+			.createResource(NS + "DramaticDifferentiatedFigure");
+	public static final Resource DramaticUndifferentiatedFigure = m_model
+			.createResource(NS + "DramaticUndifferentiatedFigure");
+	public static final Resource DramaticGroup = m_model.createResource(NS + "DramaticGroup");
 	public static final Resource GenreAssignment = m_model.createResource(NS + "GenreAssignment");
 	public static final Resource LiteraryGenre = m_model.createResource(NS + "LiteraryGenre");
+	public static final Resource LiteraryPeriod = m_model.createResource(NS + "LiteraryPeriod");
 	public static final Resource Drama = m_model.createResource(NS + "Drama");
 	public static final Resource Tragedy = m_model.createResource(NS + "Tragedy");
 	public static final Property from = m_model.createProperty(NS + "From");
-	public static final Property isAnnotated = m_model.createProperty(NS + "isAnnotated");
 	public static final Property hasPremiere = m_model.createProperty(NS + "hasPremiere");
 	public static final Property hasGenre = m_model.createProperty(NS + "hasGenre");
 	public static final Property genreAssignment = m_model.createProperty(NS + "genreAssignment");
 	public static final Property certainty = m_model.createProperty(NS + "certainty");
 	public static final Property alsoSpelledAs = m_model.createProperty(NS + "alsoSpelledAs");
+	public static final Property isClassifiedAs = m_model.createProperty(NS + "isClassifiedAs");
+	public static final Property associatedWith = m_model.createProperty(NS + "associatedWith");
+	public static final Property isRelated = m_model.createProperty(NS + "isRelated");
+	public static final Property siblingOf = m_model.createProperty(NS + "siblingOf");
+	public static final Property spouseOf = m_model.createProperty(NS + "spouseOf");
+	public static final Property childOf = m_model.createProperty(NS + "childOf");
+	public static final Property parentOf = m_model.createProperty(NS + "parentOf");
 
 }
