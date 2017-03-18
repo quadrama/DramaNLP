1ad644c38d3e5cbcfd26076201e9e82f91a30f79
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-03-18 07:34:34 +0100

    removing annotation stuff

--- de.unistuttgart.ims.drama.meta/src/test/java/de/unistuttgart/ims/drama/meta/TestMetaDataExport.java
+++ de.unistuttgart.ims.drama.meta/src/test/java/de/unistuttgart/ims/drama/meta/TestMetaDataExport.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import de.unistuttgart.quadrama.core.FigureReferenceAnnotator;
 
 public class TestMetaDataExport {
 	@Test
@@ -17,6 +19,7 @@ public class TestMetaDataExport {
 		SimplePipeline.runPipeline(
 				CollectionReaderFactory.createReaderDescription(XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
 						"src/test/resources/*.xmi", XmiReader.PARAM_LENIENT, true),
-				MetaDataExport.getDescription(new File("target/ontology.owl")));
+				AnalysisEngineFactory.createEngineDescription(FigureReferenceAnnotator.class),
+				MetaDataExport.getDescription(new File("target/ontology.owl"), true, false));
 	}
 }
