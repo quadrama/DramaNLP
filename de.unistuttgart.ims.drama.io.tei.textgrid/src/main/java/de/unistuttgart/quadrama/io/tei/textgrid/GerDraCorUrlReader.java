afde75a561e2c99af6e63467a7b95f3f349f3c50
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-03-21 04:00:45 +0100

    reusing functions from TextGridUtil

--- de.unistuttgart.ims.drama.io.tei.textgrid/src/main/java/de/unistuttgart/quadrama/io/tei/textgrid/GerDraCorUrlReader.java
+++ de.unistuttgart.ims.drama.io.tei.textgrid/src/main/java/de/unistuttgart/quadrama/io/tei/textgrid/GerDraCorUrlReader.java
@@ -4,10 +4,10 @@ import static de.unistuttgart.quadrama.io.core.DramaIOUtil.select2Annotation;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.collection.CollectionException;
@@ -24,17 +24,13 @@ import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
 import de.unistuttgart.ims.drama.api.Act;
-import de.unistuttgart.ims.drama.api.ActHeading;
 import de.unistuttgart.ims.drama.api.Author;
 import de.unistuttgart.ims.drama.api.CastFigure;
 import de.unistuttgart.ims.drama.api.Drama;
-import de.unistuttgart.ims.drama.api.DramatisPersonae;
 import de.unistuttgart.ims.drama.api.Figure;
-import de.unistuttgart.ims.drama.api.FigureDescription;
 import de.unistuttgart.ims.drama.api.FrontMatter;
 import de.unistuttgart.ims.drama.api.MainMatter;
 import de.unistuttgart.ims.drama.api.Scene;
-import de.unistuttgart.ims.drama.api.SceneHeading;
 import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Speech;
 import de.unistuttgart.ims.drama.api.StageDirection;
@@ -43,7 +39,6 @@ import de.unistuttgart.ims.uimautil.AnnotationUtil;
 import de.unistuttgart.quadrama.io.core.AbstractDramaUrlReader;
 import de.unistuttgart.quadrama.io.core.Select2AnnotationCallback;
 import de.unistuttgart.quadrama.io.core.Visitor;
-import de.unistuttgart.quadrama.io.core.type.HTMLAnnotation;
 
 public class GerDraCorUrlReader extends AbstractDramaUrlReader {
 
@@ -110,8 +105,8 @@ public class GerDraCorUrlReader extends AbstractDramaUrlReader {
 		select2Annotation(jcas, root, vis.getAnnotationMap(), "ab", Speech.class, mainMatter);
 		select2Annotation(jcas, root, vis.getAnnotationMap(), "p", Speech.class, mainMatter);
 
-		readActsAndScenes(jcas, root, vis.getAnnotationMap(), true);
-		readDramatisPersonae(jcas, root, vis.getAnnotationMap());
+		TextGridUtil.readActsAndScenes(jcas, root, vis.getAnnotationMap(), true);
+		TextGridUtil.readDramatisPersonae(jcas, root, vis.getAnnotationMap());
 
 		Map<String, CastFigure> idFigureMap = new HashMap<String, CastFigure>();
 		Elements castEntries = doc.select("profileDesc > particDesc > listPerson > person");
@@ -138,87 +133,12 @@ public class GerDraCorUrlReader extends AbstractDramaUrlReader {
 				speaker.setCastFigure(i, idFigureMap.get(speaker.getXmlId(i)));
 		}
 
-	}
+		AnnotationUtil.trim(new ArrayList<Figure>(JCasUtil.select(jcas, Figure.class)));
+		AnnotationUtil.trim(new ArrayList<Speech>(JCasUtil.select(jcas, Speech.class)));
+		AnnotationUtil.trim(new ArrayList<Utterance>(JCasUtil.select(jcas, Utterance.class)));
+		AnnotationUtil.trim(new ArrayList<Scene>(JCasUtil.select(jcas, Scene.class)));
+		AnnotationUtil.trim(new ArrayList<Act>(JCasUtil.select(jcas, Act.class)));
 
-	public void readActsAndScenes(JCas jcas, Element root, Map<String, HTMLAnnotation> map, boolean strict) {
-		readActs(jcas, root, map, strict);
-		readScenes(jcas, root, map, strict);
-
-	}
-
-	public static void readActs(JCas jcas, Element root, Map<String, HTMLAnnotation> map, boolean strict) {
-		if (!root.select("div[type=act]").isEmpty()) {
-			for (Act a : select2Annotation(jcas, root, map, "div[type=act]", Act.class, null)) {
-				a.setRegular(true);
-			}
-
-			select2Annotation(jcas, root, map, "div[type=act] > div > desc > title", ActHeading.class, null);
-			select2Annotation(jcas, root, map, "div[type=act] > div > head", ActHeading.class, null);
-
-			for (Act prol : select2Annotation(jcas, root, map, "div[type=prologue]", Act.class, null)) {
-				prol.setRegular(false);
-			}
-
-		}
-		if (!strict && !JCasUtil.exists(jcas, Act.class)) {
-			select2Annotation(jcas, root, map, "body > div", Act.class, null);
-			select2Annotation(jcas, root, map, "body > div > head", ActHeading.class, null);
-			select2Annotation(jcas, root, map, "body > div > desc > title", ActHeading.class, null);
-		}
-	}
-
-	public static void readScenes(JCas jcas, Element root, Map<String, HTMLAnnotation> map, boolean strict) {
-		if (!root.select("div[type=scene]").isEmpty()) {
-			select2Annotation(jcas, root, map, "div[type=scene]", Scene.class, null);
-			select2Annotation(jcas, root, map, "div[type=scene] > div > desc > title", SceneHeading.class, null);
-		} else if (!strict) {
-			if (JCasUtil.exists(jcas, Act.class))
-				for (Act act : JCasUtil.select(jcas, Act.class)) {
-
-					Collection<Scene> scenes = select2Annotation(jcas, root, map, "body > div > div:has(head)",
-							Scene.class, act);
-					for (Scene sc : scenes) {
-						select2Annotation(jcas, root, map, "div > desc > title", SceneHeading.class, sc);
-					}
-				}
-			else {
-				select2Annotation(jcas, root, map, "body > div > div:not(:has(desc > title))", Scene.class, null);
-				select2Annotation(jcas, root, map, "body > div > div > head", SceneHeading.class, null);
-				select2Annotation(jcas, root, map, "body > div > div > desc > title", SceneHeading.class, null);
-			}
-		}
-	}
-
-	public static void readDramatisPersonae(JCas jcas, Element root, Map<String, HTMLAnnotation> map) {
-		DramatisPersonae dp;
-		if (!root.select("castList").isEmpty()) {
-			dp = select2Annotation(jcas, root, map, "castList", DramatisPersonae.class, null).iterator().next();
-
-			Element castList = root.select("castList").first();
-			if (!castList.select("castItem role").isEmpty()) {
-				select2Annotation(jcas, castList, map, "castItem role", Figure.class, dp);
-				Collection<FigureDescription> figDescs = select2Annotation(jcas, castList, map, "castItem roleDesc",
-						FigureDescription.class, dp);
-				for (FigureDescription figureDescription : figDescs) {
-					Figure fig = JCasUtil.selectPreceding(Figure.class, figureDescription, 1).get(0);
-					fig.setDescription(figureDescription);
-				}
-			} else {
-				select2Annotation(jcas, castList, map, "castItem", Figure.class, null);
-				// fixFigureAnnotations(jcas);
-			}
-		} else {
-			try {
-				dp = select2Annotation(jcas, root, map, "div[type=front] > div:has(p)", DramatisPersonae.class, null)
-						.iterator().next();
-
-				AnnotationUtil.trim(select2Annotation(jcas, root, map, "p", Figure.class, dp));
-				// fixFigureAnnotations(jcas);
-			} catch (NoSuchElementException e) {
-				System.err.println("No dramatis personae annotation in drama " + Drama.get(jcas).getDocumentId());
-				// e.printStackTrace();
-			}
-		}
 	}
 
 }
