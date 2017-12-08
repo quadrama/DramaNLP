18562754cdefd4af286cd515c92779bdf04b4abe
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-12-08 17:55:42 +0100

    We use standard coref chains

--- de.unistuttgart.ims.drama.main/src/main/java/de/unistuttgart/ims/drama/main/annotation/WebAnnoPreparation.java
+++ de.unistuttgart.ims.drama.main/src/main/java/de/unistuttgart/ims/drama/main/annotation/WebAnnoPreparation.java
@@ -6,20 +6,25 @@ import org.apache.uima.fit.factory.AnnotationFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
+import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceChain;
+import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceLink;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.unistuttgart.ims.drama.api.ActHeading;
 import de.unistuttgart.ims.drama.api.SceneHeading;
 import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Speech;
 import de.unistuttgart.ims.drama.api.StageDirection;
-import webanno.custom.RA;
 
 public class WebAnnoPreparation extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		for (Speaker speaker : JCasUtil.select(jcas, Speaker.class)) {
-			AnnotationFactory.createAnnotation(jcas, speaker.getBegin(), speaker.getEnd(), RA.class);
+			CoreferenceLink cl = AnnotationFactory.createAnnotation(jcas, speaker.getBegin(), speaker.getEnd(),
+					CoreferenceLink.class);
+			CoreferenceChain cc = new CoreferenceChain(jcas);
+			cc.setFirst(cl);
+			cc.addToIndexes();
 			AnnotationFactory.createAnnotation(jcas, speaker.getBegin(), speaker.getEnd(), Sentence.class);
 		}
 
