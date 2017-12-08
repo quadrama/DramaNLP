fe96864937d3180c9baae0a548f455e4d8a63d5d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-12-08 17:57:26 +0100

    Map coref chains to HTMLAnnotations

---
+++ de.unistuttgart.ims.drama.main/src/main/java/de/unistuttgart/ims/drama/main/annotation/UIMA2TEI.java
@@ -0,0 +1,46 @@
+package de.unistuttgart.ims.drama.main.annotation;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceChain;
+import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceLink;
+import de.unistuttgart.ims.drama.api.Speaker;
+import de.unistuttgart.quadrama.io.core.type.HTMLAnnotation;
+
+public class UIMA2TEI extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+		Map<CoreferenceLink, Collection<Speaker>> index = JCasUtil.indexCovering(jcas, CoreferenceLink.class,
+				Speaker.class);
+
+		int id = 0;
+		HTMLAnnotation h;
+		for (CoreferenceChain cc : JCasUtil.select(jcas, CoreferenceChain.class)) {
+			CoreferenceLink link = cc.getFirst();
+
+			String xmlId = null;
+			while (link != null) {
+				if (!index.get(link).isEmpty()) {
+					if (xmlId == null)
+						xmlId = index.get(link).iterator().next().getXmlId(0);
+				} else {
+					h = AnnotationFactory.createAnnotation(jcas, link.getBegin(), link.getEnd(), HTMLAnnotation.class);
+					h.setTag("rs");
+					h.setAttributes(" ref=\"#" + (xmlId != null ? xmlId : id) + "\"");
+				}
+				link = link.getNext();
+			}
+			id++;
+		}
+	}
+
+}
