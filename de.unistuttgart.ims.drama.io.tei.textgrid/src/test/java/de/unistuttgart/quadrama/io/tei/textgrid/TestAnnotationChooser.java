f2ce63347e108695c93a998a1ff08fafacb0bc13
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-12-08 17:56:42 +0100

    Comparator generate XML in the right order

---
+++ de.unistuttgart.ims.drama.io.tei.textgrid/src/test/java/de/unistuttgart/quadrama/io/tei/textgrid/TestAnnotationChooser.java
@@ -0,0 +1,28 @@
+package de.unistuttgart.quadrama.io.tei.textgrid;
+
+import java.util.TreeSet;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.AnnotationFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.junit.Test;
+
+public class TestAnnotationChooser {
+
+	@Test
+	public void testChooser() throws UIMAException {
+		JCas jcas = JCasFactory.createText("the dog barks");
+
+		TreeSet<Annotation> ts = new TreeSet<Annotation>(new AnnotationChooser(5));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 5, 5, Annotation.class));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 5, 10, Annotation.class));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 1, 5, Annotation.class));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 5, 8, Annotation.class));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 5, 5, Annotation.class));
+		ts.add(AnnotationFactory.createAnnotation(jcas, 4, 5, Annotation.class));
+
+		System.out.println(ts);
+	}
+}
