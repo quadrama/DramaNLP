9af897644c1082323b7968e9676a3a68877ac455
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-12-08 19:01:58 +0100

    Speakers are pre-chained.

--- de.unistuttgart.ims.drama.main/src/main/java/de/unistuttgart/ims/drama/main/annotation/WebAnnoPreparation.java
+++ de.unistuttgart.ims.drama.main/src/main/java/de/unistuttgart/ims/drama/main/annotation/WebAnnoPreparation.java
@@ -1,5 +1,8 @@
 package de.unistuttgart.ims.drama.main.annotation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.factory.AnnotationFactory;
@@ -10,6 +13,8 @@ import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceChain;
 import de.tudarmstadt.ukp.dkpro.core.api.coref.type.CoreferenceLink;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.unistuttgart.ims.drama.api.ActHeading;
+import de.unistuttgart.ims.drama.api.CastFigure;
+import de.unistuttgart.ims.drama.api.Drama;
 import de.unistuttgart.ims.drama.api.SceneHeading;
 import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Speech;
@@ -19,13 +24,35 @@ public class WebAnnoPreparation extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		Drama d = JCasUtil.selectSingle(jcas, Drama.class);
+		Map<CastFigure, CoreferenceChain> chains = new HashMap<CastFigure, CoreferenceChain>();
+		Map<CastFigure, CoreferenceLink> lastLink = new HashMap<CastFigure, CoreferenceLink>();
+		for (int i = 0; i < d.getCastList().size(); i++) {
+			CoreferenceChain cc = new CoreferenceChain(jcas);
+			cc.addToIndexes();
+			chains.put(d.getCastList(i), cc);
+		}
+
 		for (Speaker speaker : JCasUtil.select(jcas, Speaker.class)) {
 			CoreferenceLink cl = AnnotationFactory.createAnnotation(jcas, speaker.getBegin(), speaker.getEnd(),
 					CoreferenceLink.class);
-			CoreferenceChain cc = new CoreferenceChain(jcas);
-			cc.setFirst(cl);
-			cc.addToIndexes();
+			if (speaker.getCastFigure().size() == 1) {
+				CastFigure cf = speaker.getCastFigure(0);
+				CoreferenceChain cc = chains.get(cf);
+				if (cc.getFirst() == null) {
+					cc.setFirst(cl);
+				} else {
+					lastLink.get(cf).setNext(cl);
+				}
+				lastLink.put(cf, cl);
+
+			} else {
+				CoreferenceChain cc = new CoreferenceChain(jcas);
+				cc.setFirst(cl);
+				cc.addToIndexes();
+			}
 			AnnotationFactory.createAnnotation(jcas, speaker.getBegin(), speaker.getEnd(), Sentence.class);
+
 		}
 
 		for (Speech speaker : JCasUtil.select(jcas, Speech.class)) {
