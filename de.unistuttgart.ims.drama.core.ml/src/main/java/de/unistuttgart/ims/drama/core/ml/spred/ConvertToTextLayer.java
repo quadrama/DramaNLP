8b0acc5bfcca6969cce6ae4b4abb5f7552ae3d06
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-01-10 20:04:28 +0100

    Added Act and Scene headings #26

--- de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/spred/ConvertToTextLayer.java
+++ de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/spred/ConvertToTextLayer.java
@@ -5,6 +5,8 @@ import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import de.unistuttgart.ims.drama.api.ActHeading;
+import de.unistuttgart.ims.drama.api.SceneHeading;
 import de.unistuttgart.ims.drama.api.Speaker;
 import de.unistuttgart.ims.drama.api.Speech;
 import de.unistuttgart.ims.drama.api.StageDirection;
@@ -24,6 +26,14 @@ public class ConvertToTextLayer {
 				clazz, MapAnnotations.PARAM_TARGET_CLASS, TextLayer.class, MapAnnotations.PARAM_FEATURE_NAME, "Name",
 				MapAnnotations.PARAM_FEATURE_VALUE, clazz.getSimpleName()));
 		clazz = StageDirection.class;
+		b.add(AnalysisEngineFactory.createEngineDescription(MapAnnotations.class, MapAnnotations.PARAM_SOURCE_CLASS,
+				clazz, MapAnnotations.PARAM_TARGET_CLASS, TextLayer.class, MapAnnotations.PARAM_FEATURE_NAME, "Name",
+				MapAnnotations.PARAM_FEATURE_VALUE, clazz.getSimpleName()));
+		clazz = SceneHeading.class;
+		b.add(AnalysisEngineFactory.createEngineDescription(MapAnnotations.class, MapAnnotations.PARAM_SOURCE_CLASS,
+				clazz, MapAnnotations.PARAM_TARGET_CLASS, TextLayer.class, MapAnnotations.PARAM_FEATURE_NAME, "Name",
+				MapAnnotations.PARAM_FEATURE_VALUE, clazz.getSimpleName()));
+		clazz = ActHeading.class;
 		b.add(AnalysisEngineFactory.createEngineDescription(MapAnnotations.class, MapAnnotations.PARAM_SOURCE_CLASS,
 				clazz, MapAnnotations.PARAM_TARGET_CLASS, TextLayer.class, MapAnnotations.PARAM_FEATURE_NAME, "Name",
 				MapAnnotations.PARAM_FEATURE_VALUE, clazz.getSimpleName()));
