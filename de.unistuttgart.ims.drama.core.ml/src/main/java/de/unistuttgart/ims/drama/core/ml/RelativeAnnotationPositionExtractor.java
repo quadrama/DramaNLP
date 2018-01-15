32afe72060dce6a3d984db64be58fbcd0378cf11
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-01-15 11:03:24 +0100

    Fixed relative position feature

--- de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/RelativeAnnotationPositionExtractor.java
+++ de.unistuttgart.ims.drama.core.ml/src/main/java/de/unistuttgart/ims/drama/core/ml/RelativeAnnotationPositionExtractor.java
@@ -7,9 +7,9 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.cleartk.ml.Feature;
 import org.cleartk.ml.feature.extractor.CleartkExtractorException;
-import org.cleartk.ml.feature.extractor.NamedFeatureExtractor1;
+import org.cleartk.ml.feature.extractor.FeatureExtractor1;
 
-public class RelativeAnnotationPositionExtractor<T extends Annotation> implements NamedFeatureExtractor1<T> {
+public class RelativeAnnotationPositionExtractor<T extends Annotation> implements FeatureExtractor1<T> {
 	private static final String FEATURE_NAME = "relative-position";
 
 	@Override
@@ -17,12 +17,11 @@ public class RelativeAnnotationPositionExtractor<T extends Annotation> implement
 		List<Feature> features = new ArrayList<Feature>();
 
 		double rpos = focusAnnotation.getBegin() / (double) view.getDocumentText().length();
-		Feature f = new Feature(getFeatureName(), rpos);
+		Feature f = new Feature(getFeatureName() + "_" + String.valueOf(Math.round(rpos * 1000)), rpos * 1000);
 		features.add(f);
 		return features;
 	}
 
-	@Override
 	public String getFeatureName() {
 		return FEATURE_NAME;
 	}
