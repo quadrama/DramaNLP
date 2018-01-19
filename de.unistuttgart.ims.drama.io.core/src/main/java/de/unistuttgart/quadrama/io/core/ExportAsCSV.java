bc0b445db1c3f0654e925178ef40831ad75f4c19
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-01-19 11:32:26 +0100

    Added documentation

--- de.unistuttgart.ims.drama.io.core/src/main/java/de/unistuttgart/quadrama/io/core/ExportAsCSV.java
+++ de.unistuttgart.ims.drama.io.core/src/main/java/de/unistuttgart/quadrama/io/core/ExportAsCSV.java
@@ -15,9 +15,25 @@ import org.apache.uima.resource.ResourceInitializationException;
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 
+/**
+ * This exporter generates the base CSV files we analyse. Documentation etc.
+ * uses the term "CSV", although the exact format (CSV vs. TSV) can be
+ * parameterized.
+ * 
+ * @since 1.0.0
+ */
 public class ExportAsCSV extends JCasFileWriter_ImplBase {
 
+	/**
+	 * The CSV variant controls what is contained in the output. See
+	 * {@link de.unistuttgart.quadrama.io.core.CSVVariant} for details.
+	 */
 	public static final String PARAM_CSV_VARIANT_NAME = "CSV Variant Name";
+
+	/**
+	 * The exact format. See {@link org.apache.commons.csv.CSVFormat} for
+	 * details.
+	 */
 	public static final String PARAM_CSV_FORMAT_NAME = "CSV Format Name";
 
 	@ConfigurationParameter(name = PARAM_CSV_VARIANT_NAME, defaultValue = "UtterancesWithTokens")
