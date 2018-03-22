573708215ae829390afdda7325701c80cfc8b33f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-22 09:53:30 +0100

    Added preparing of resources

--- de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestSpeakerAssignmentRules.java
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestSpeakerAssignmentRules.java
@@ -13,14 +13,34 @@ import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.unistuttgart.ims.drama.api.Speaker;
+import de.unistuttgart.quadrama.io.tei.GerDraCorReader;
 import de.unistuttgart.quadrama.io.tei.TextgridTEIUrlReader;
 
 public class TestSpeakerAssignmentRules {
+
+	@BeforeClass
+	public static void prepareResources() throws ResourceInitializationException, UIMAException, IOException {
+		SimplePipeline.runPipeline(
+				CollectionReaderFactory.createReaderDescription(GerDraCorReader.class, TextgridTEIUrlReader.PARAM_INPUT,
+						"src/test/resources/tei/tx4z.0.xml", TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
+				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
+						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
+
+		SimplePipeline.runPipeline(
+				CollectionReaderFactory.createReaderDescription(GerDraCorReader.class, TextgridTEIUrlReader.PARAM_INPUT,
+						"src/test/resources/tei/w3zd.0.xml", TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
+				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
+						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
+
+		new File("src/test/resources/SpeakerAssignmentRules/typesystem.xml").delete();
+	}
+
 	@Test
 	public void testRules1() throws Exception {
 		org.apache.uima.fit.pipeline.JCasIterator iter = SimplePipeline.iteratePipeline(
@@ -77,24 +97,6 @@ public class TestSpeakerAssignmentRules {
 			if (speaker.getCoveredText().equalsIgnoreCase("der capitain"))
 				assertNotNull(speaker.getCoveredText(), speaker.getFigure());
 		}
-	}
-
-	public static void main(String[] args) throws ResourceInitializationException, UIMAException, IOException {
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
-						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:tx4z.0",
-						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
-
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
-						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:w3zd.0",
-						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
-
-		new File("src/test/resources/SpeakerAssignmentRules/typesystem.xml").delete();
 	}
 
 }
