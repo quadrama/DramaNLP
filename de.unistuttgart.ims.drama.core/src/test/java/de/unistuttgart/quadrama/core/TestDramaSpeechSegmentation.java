37a2649f872cb6a82908e79af559473a0b997e1c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-03-26 09:48:13 +0200

    We now run this as needed manually

--- de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestDramaSpeechSegmentation.java
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestDramaSpeechSegmentation.java
@@ -17,7 +17,6 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -53,13 +52,12 @@ public class TestDramaSpeechSegmentation {
 		assertEquals("ruhig", JCasUtil.selectByIndex(jcas, Token.class, 20).getCoveredText());
 	}
 
-	@BeforeClass
-	public static void prepareResources() throws ResourceInitializationException, UIMAException, IOException {
+	public static void main(String[] args) throws ResourceInitializationException, UIMAException, IOException {
 		SimplePipeline.runPipeline(
 				CollectionReaderFactory.createReaderDescription(GerDraCorReader.class, TextgridTEIUrlReader.PARAM_INPUT,
 						"src/test/resources/tei/rfxf.0.xml", TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
 				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/DramaSpeechSegmenter/"));
+						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/DramaSpeechSegmenter"));
 
 	}
 }
