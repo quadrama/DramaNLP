19004ad1ccbb60464f8f2677595955cc43b0d1ae
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-08 13:35:50 +0200

    Using generated files

--- de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestFigureReferenceAnnotator.java
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestFigureReferenceAnnotator.java
@@ -4,10 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
@@ -20,16 +16,14 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
-import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.unistuttgart.ims.drama.api.Figure;
-import de.unistuttgart.quadrama.io.tei.TextgridTEIUrlReader;
 
 public class TestFigureReferenceAnnotator {
 	@Test
 	public void testFigureReferenceAnnotator1() throws ResourceInitializationException {
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
-				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/FigureReferenceAnnotator/rfxf.0.xmi",
-				XmiReader.PARAM_LENIENT, true);
+				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/level-1/rfxf.0.xmi", XmiReader.PARAM_LENIENT,
+				true);
 		AnalysisEngineDescription engine = AnalysisEngineFactory
 				.createEngineDescription(FigureReferenceAnnotator.class);
 		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
@@ -46,8 +40,8 @@ public class TestFigureReferenceAnnotator {
 	@Test
 	public void testFigureReferenceAnnotator2() throws ResourceInitializationException {
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
-				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/FigureReferenceAnnotator/w3zd.0.xmi",
-				XmiReader.PARAM_LENIENT, true);
+				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/level-1/w3zd.0.xmi", XmiReader.PARAM_LENIENT,
+				true);
 		AnalysisEngineDescription engine = AnalysisEngineFactory
 				.createEngineDescription(FigureReferenceAnnotator.class);
 		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
@@ -58,23 +52,5 @@ public class TestFigureReferenceAnnotator {
 				assertNotNull(figure.getReference());
 			}
 		}
-	}
-
-	public static void main(String[] args) throws ResourceInitializationException, UIMAException, IOException {
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
-						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:rfxf.0",
-						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/FigureReferenceAnnotator/"));
-
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(TextgridTEIUrlReader.class,
-						TextgridTEIUrlReader.PARAM_INPUT, "http://www.textgridrep.org/textgrid:w3zd.0",
-						TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/FigureReferenceAnnotator/"));
-
-		new File("src/test/resources/FigureReferenceAnnotator/typesystem.xml").delete();
 	}
 }
--- de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestSpeakerAssignmentRules.java
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestSpeakerAssignmentRules.java
@@ -4,46 +4,24 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.apache.uima.UIMAException;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
-import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiWriter;
 import de.unistuttgart.ims.drama.api.Speaker;
-import de.unistuttgart.quadrama.io.tei.GerDraCorReader;
-import de.unistuttgart.quadrama.io.tei.TextgridTEIUrlReader;
 
 public class TestSpeakerAssignmentRules {
-
-	public static void main(String[] args) throws ResourceInitializationException, UIMAException, IOException {
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(GerDraCorReader.class, TextgridTEIUrlReader.PARAM_INPUT,
-						"src/test/resources/tei/tx4z.0.xml", TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
-
-		SimplePipeline.runPipeline(
-				CollectionReaderFactory.createReaderDescription(GerDraCorReader.class, TextgridTEIUrlReader.PARAM_INPUT,
-						"src/test/resources/tei/w3zd.0.xml", TextgridTEIUrlReader.PARAM_REMOVE_XML_ANNOTATIONS, true),
-				AnalysisEngineFactory.createEngineDescription(XmiWriter.class, XmiWriter.PARAM_USE_DOCUMENT_ID, true,
-						XmiWriter.PARAM_TARGET_LOCATION, "src/test/resources/SpeakerAssignmentRules/"));
-
-		new File("src/test/resources/SpeakerAssignmentRules/typesystem.xml").delete();
-	}
 
 	@Test
 	public void testRules1() throws Exception {
 		org.apache.uima.fit.pipeline.JCasIterator iter = SimplePipeline.iteratePipeline(
 				CollectionReaderFactory.createReaderDescription(XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
-						"src/test/resources/SpeakerAssignmentRules/tx4z.0.xmi", XmiReader.PARAM_LENIENT, true),
+						"src/test/resources/level-1/tx4z.0.xmi", XmiReader.PARAM_LENIENT, true),
 				AnalysisEngineFactory.createEngineDescription(FigureReferenceAnnotator.class),
 				AnalysisEngineFactory.createEngineDescription(SpeakerAssignmentRules.class,
 						SpeakerAssignmentRules.PARAM_RULE_FILE_URL,
@@ -63,7 +41,7 @@ public class TestSpeakerAssignmentRules {
 	public void testRules2() throws Exception {
 		org.apache.uima.fit.pipeline.JCasIterator iter = SimplePipeline.iteratePipeline(
 				CollectionReaderFactory.createReaderDescription(XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
-						"src/test/resources/SpeakerAssignmentRules/w3zd.0.xmi", XmiReader.PARAM_LENIENT, true),
+						"src/test/resources/level-1/w3zd.0.xmi", XmiReader.PARAM_LENIENT, true),
 				AnalysisEngineFactory.createEngineDescription(FigureReferenceAnnotator.class),
 				AnalysisEngineFactory.createEngineDescription(SpeakerAssignmentRules.class,
 						SpeakerAssignmentRules.PARAM_RULE_FILE_URL,
@@ -82,7 +60,7 @@ public class TestSpeakerAssignmentRules {
 	public void testRules3() throws Exception {
 		org.apache.uima.fit.pipeline.JCasIterator iter = SimplePipeline.iteratePipeline(
 				CollectionReaderFactory.createReaderDescription(XmiReader.class, XmiReader.PARAM_SOURCE_LOCATION,
-						"src/test/resources/SpeakerAssignmentRules/w3zd.0.xmi", XmiReader.PARAM_LENIENT, true),
+						"src/test/resources/level-1/w3zd.0.xmi", XmiReader.PARAM_LENIENT, true),
 				AnalysisEngineFactory.createEngineDescription(FigureReferenceAnnotator.class),
 				AnalysisEngineFactory.createEngineDescription(SpeakerAssignmentRules.class,
 						SpeakerAssignmentRules.PARAM_RULE_FILE_URL,
