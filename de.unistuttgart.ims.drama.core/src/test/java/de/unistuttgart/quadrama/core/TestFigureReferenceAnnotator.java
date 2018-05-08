8e25e26874bf50e7526da609b7d0dd1d105b682c
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-05-08 15:38:12 +0200

    file access

--- de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestFigureReferenceAnnotator.java
+++ de.unistuttgart.ims.drama.core/src/test/java/de/unistuttgart/quadrama/core/TestFigureReferenceAnnotator.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
@@ -13,17 +15,24 @@ import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import de.unistuttgart.ims.drama.api.Figure;
 
 public class TestFigureReferenceAnnotator {
+	File directory;
+
+	@Before
+	public void setUp() {
+		directory = new File(getClass().getResource("/level-1").getFile());
+	}
+
 	@Test
 	public void testFigureReferenceAnnotator1() throws ResourceInitializationException {
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
-				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/level-1/rfxf.0.xmi", XmiReader.PARAM_LENIENT,
-				true);
+				XmiReader.PARAM_SOURCE_LOCATION, new File(directory, "rfxf.0.xmi"), XmiReader.PARAM_LENIENT, true);
 		AnalysisEngineDescription engine = AnalysisEngineFactory
 				.createEngineDescription(FigureReferenceAnnotator.class);
 		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
@@ -40,8 +49,7 @@ public class TestFigureReferenceAnnotator {
 	@Test
 	public void testFigureReferenceAnnotator2() throws ResourceInitializationException {
 		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(XmiReader.class,
-				XmiReader.PARAM_SOURCE_LOCATION, "src/test/resources/level-1/w3zd.0.xmi", XmiReader.PARAM_LENIENT,
-				true);
+				XmiReader.PARAM_SOURCE_LOCATION, new File(directory, "w3zd.0.xmi"), XmiReader.PARAM_LENIENT, true);
 		AnalysisEngineDescription engine = AnalysisEngineFactory
 				.createEngineDescription(FigureReferenceAnnotator.class);
 		JCasIterator iterator = SimplePipeline.iteratePipeline(reader, engine).iterator();
